1f30d50418eb41f5f778017e63465597f0e59467
Lucas Clemente <lucas@clemente.io>
2016-04-08 12:10:21 +0200

    move readUint64 to new file utils.go

--- public_header.go
+++ public_header.go
@@ -70,15 +70,3 @@ func ParsePublicHeader(b io.ByteReader) (*PublicHeader, error) {
 
 	return header, nil
 }
-
-func readUint64(b io.ByteReader, length uint8) (uint64, error) {
-	var res uint64
-	for i := uint8(0); i < length; i++ {
-		bt, err := b.ReadByte()
-		if err != nil {
-			return 0, err
-		}
-		res = res<<8 + uint64(bt)
-	}
-	return res, nil
-}
