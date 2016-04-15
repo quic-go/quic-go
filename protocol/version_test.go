30d5766598ed9970cf97b7a9e66f9973a07e0bff
Lucas Clemente <lucas@clemente.io>
2016-04-15 20:40:39 +0200

    improve version tests

--- protocol/version_test.go
+++ protocol/version_test.go
@@ -10,9 +10,11 @@ import (
 var _ = Describe("Version", func() {
 	It("converts tags to numbers", func() {
 		Expect(protocol.VersionTagToNumber('Q' + '1'<<8 + '2'<<16 + '3'<<24)).To(Equal(protocol.VersionNumber(123)))
+		Expect(protocol.VersionTagToNumber('Q' + '0'<<8 + '3'<<16 + '0'<<24)).To(Equal(protocol.VersionNumber(30)))
 	})
 
 	It("converts number to tag", func() {
 		Expect(protocol.VersionNumberToTag(protocol.VersionNumber(123))).To(Equal(uint32('Q' + '1'<<8 + '2'<<16 + '3'<<24)))
+		Expect(protocol.VersionNumberToTag(protocol.VersionNumber(30))).To(Equal(uint32('Q' + '0'<<8 + '3'<<16 + '0'<<24)))
 	})
 })
