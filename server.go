0743c0824ff3f7f012f1632f77cfbec9bb88453e
Lucas Clemente <lucas@clemente.io>
2016-05-15 14:39:36 +0200

    add version number to server log

--- server.go
+++ server.go
@@ -119,7 +119,7 @@ func (s *Server) handlePacket(conn *net.UDPConn, remoteAddr *net.UDPAddr, packet
 	s.sessionsMutex.RUnlock()
 
 	if !ok {
-		utils.Infof("Serving new connection: %x from %v", publicHeader.ConnectionID, remoteAddr)
+		utils.Infof("Serving new connection: %x, version %d from %v", publicHeader.ConnectionID, publicHeader.VersionNumber, remoteAddr)
 		session = s.newSession(
 			&udpConn{conn: conn, currentAddr: remoteAddr},
 			publicHeader.VersionNumber,
