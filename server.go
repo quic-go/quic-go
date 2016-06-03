c0b73c4beb0bab76be4ac2be768a1181d36d4905
Lucas Clemente <lucas@clemente.io>
2016-06-03 12:13:07 +0200

    fix a potential race in server.Close()

--- server.go
+++ server.go
@@ -109,15 +109,14 @@ func (s *Server) Close() error {
 	s.sessionsMutex.Unlock()
 
 	s.connMutex.Lock()
-	defer s.connMutex.Unlock()
+	conn := s.conn
+	s.conn = nil
+	s.connMutex.Unlock()
 
-	if s.conn == nil {
+	if conn == nil {
 		return nil
 	}
-	defer func() {
-		s.conn = nil
-	}()
-	return s.conn.Close()
+	return conn.Close()
 }
 
 func (s *Server) handlePacket(conn *net.UDPConn, remoteAddr *net.UDPAddr, packet []byte) error {
