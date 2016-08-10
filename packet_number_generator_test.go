c7e3b34e2d789758183ec1cd078e6ad42b9a6b21
Marten Seemann <martenseemann@gmail.com>
2016-08-10 18:52:27 +0700

    implement a PacketNumberGenerator
    ref #186

---
+++ packet_number_generator_test.go
@@ -0,0 +1,71 @@
+package quic
+
+import (
+	"math"
+
+	"github.com/lucas-clemente/quic-go/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Packet Number Generator", func() {
+	var png packetNumberGenerator
+
+	BeforeEach(func() {
+		png = packetNumberGenerator{}
+	})
+
+	It("gets 1 as the first packet number", func() {
+		num := png.GetNextPacketNumber()
+		Expect(num).To(Equal(protocol.PacketNumber(1)))
+	})
+
+	It("skips a packet number", func() {
+		png.nextToSkip = 2
+		num := png.GetNextPacketNumber()
+		Expect(num).To(Equal(protocol.PacketNumber(1)))
+		num = png.GetNextPacketNumber()
+		Expect(num).To(Equal(protocol.PacketNumber(3)))
+	})
+
+	It("generates a new packet number to skip", func() {
+		png.last = 100
+		png.averagePeriod = 100
+
+		rep := 1000
+		var sum protocol.PacketNumber
+
+		for i := 0; i < rep; i++ {
+			png.generateNewSkip()
+			Expect(png.nextToSkip).ToNot(Equal(protocol.PacketNumber(101)))
+			sum += png.nextToSkip
+		}
+
+		average := sum / protocol.PacketNumber(rep)
+		Expect(average).To(BeNumerically("==", protocol.PacketNumber(200), 10))
+	})
+
+	It("uses random numbers", func() {
+		var smallest uint16 = math.MaxUint16
+		var largest uint16
+		var sum uint64
+
+		rep := 10000
+
+		for i := 0; i < rep; i++ {
+			num, err := png.getRandomNumber()
+			Expect(err).ToNot(HaveOccurred())
+			sum += uint64(num)
+			if num > largest {
+				largest = num
+			}
+			if num < smallest {
+				smallest = num
+			}
+		}
+
+		Expect(smallest).To(BeNumerically("<", 300))
+		Expect(largest).To(BeNumerically(">", math.MaxUint16-300))
+		Expect(sum / uint64(rep)).To(BeNumerically("==", uint64(math.MaxUint16/2), 1000))
+	})
+})
