327856e31c00f3d09368197ce85ac9421f7f3b35
Carlos Martín Nieto <cmn@dwim.me>
2016-11-12 13:36:44 +0100

    Don't rely on GOPATH to load the certificates or error codes
    GOPATH is a list of paths, similar to PATH. If someone does have a list
    set, the tests will try to use the full list as a path prefix to load
    the certificates, which won't work.
    
    But even handling GOPATH as a list does not solve the issue of what
    certificate or error_codes.go file to load, since the code might not be
    under the first one it finds.
    
    Instead, use the runtime functionality to get the filename of the path
    of the project at compilation time and perform the lookups relative to
    that, which guarantees that we're loading the files from the path of the
    code that is running.

--- testdata/cert.go
+++ testdata/cert.go
@@ -2,19 +2,30 @@ package testdata
 
 import (
 	"crypto/tls"
-	"os"
+	"path"
+	"runtime"
 )
 
 var certPath string
 
 func init() {
-	certPath = os.Getenv("GOPATH")
-	certPath += "/src/github.com/lucas-clemente/quic-go/example"
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("Failed to get current frame")
+	}
+
+	certPath = path.Join(path.Dir(path.Dir(filename)), "example")
+}
+
+// GetCertificatePaths returns the paths to 'fullchain.pem' and 'privkey.pem' for the
+// quic.clemente.io cert.
+func GetCertificatePaths() (string, string) {
+	return path.Join(certPath, "fullchain.pem"), path.Join(certPath, "privkey.pem")
 }
 
 // GetTLSConfig returns a tls config for quic.clemente.io
 func GetTLSConfig() *tls.Config {
-	cert, err := tls.LoadX509KeyPair(certPath+"/fullchain.pem", certPath+"/privkey.pem")
+	cert, err := tls.LoadX509KeyPair(GetCertificatePaths())
 	if err != nil {
 		panic(err)
 	}
@@ -25,7 +36,7 @@ func GetTLSConfig() *tls.Config {
 
 // GetCertificate returns a certificate for quic.clemente.io
 func GetCertificate() tls.Certificate {
-	cert, err := tls.LoadX509KeyPair(certPath+"/fullchain.pem", certPath+"/privkey.pem")
+	cert, err := tls.LoadX509KeyPair(GetCertificatePaths())
 	if err != nil {
 		panic(err)
 	}
