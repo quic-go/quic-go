bbbc24c43199496a14a477206582861c18643dc6
Marten Seemann <martenseemann@gmail.com>
2016-11-28 11:27:54 +0700

    improve logging of StopWaitingFrames

--- frames/log.go
+++ frames/log.go
@@ -11,8 +11,16 @@ func LogFrame(frame Frame, sent bool) {
 	if sent {
 		dir = "->"
 	}
-	if sf, ok := frame.(*StreamFrame); ok {
-		utils.Debugf("\t%s &frames.StreamFrame{StreamID: %d, FinBit: %t, Offset: 0x%x, Data length: 0x%x, Offset + Data length: 0x%x}", dir, sf.StreamID, sf.FinBit, sf.Offset, sf.DataLen(), sf.Offset+sf.DataLen())
+	if f, ok := frame.(*StreamFrame); ok {
+		utils.Debugf("\t%s &frames.StreamFrame{StreamID: %d, FinBit: %t, Offset: 0x%x, Data length: 0x%x, Offset + Data length: 0x%x}", dir, f.StreamID, f.FinBit, f.Offset, f.DataLen(), f.Offset+f.DataLen())
+		return
+	}
+	if f, ok := frame.(*StopWaitingFrame); ok {
+		if sent {
+			utils.Debugf("\t%s &frames.StopWaitingFrame{LeastUnacked: 0x%x, PacketNumberLen: 0x%x}", dir, f.LeastUnacked, f.PacketNumberLen)
+		} else {
+			utils.Debugf("\t%s &frames.StopWaitingFrame{LeastUnacked: 0x%x}", dir, f.LeastUnacked)
+		}
 		return
 	}
 	utils.Debugf("\t%s %#v", dir, frame)
