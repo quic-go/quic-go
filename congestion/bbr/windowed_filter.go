79ec62ade9adb91c9e75642c03f8b7506eb41a0c
Marten Seemann <martenseemann@gmail.com>
2016-11-30 00:32:29 +0700

    implement windowed filter as a max filter for bandwidths

---
+++ congestion/bbr/windowed_filter.go
@@ -0,0 +1,133 @@
+package bbr
+
+import (
+	"time"
+
+	"github.com/lucas-clemente/quic-go/protocol"
+)
+
+type sample struct {
+	bandwidth protocol.Bandwidth
+	time      time.Time
+}
+
+// This only implements the maxFilter, for bandwidths
+
+// Implements Kathleen Nichols' algorithm for tracking the minimum (or maximum)
+// estimate of a stream of samples over some fixed time interval. (E.g.,
+// the minimum RTT over the past five minutes.) The algorithm keeps track of
+// the best, second best, and third best min (or max) estimates, maintaining an
+// invariant that the measurement time of the n'th best >= n-1'th best.
+
+// The algorithm works as follows. On a reset, all three estimates are set to
+// the same sample. The second best estimate is then recorded in the second
+// quarter of the window, and a third best estimate is recorded in the second
+// half of the window, bounding the worst case error when the true min is
+// monotonically increasing (or true max is monotonically decreasing) over the
+// window.
+//
+// A new best sample replaces all three estimates, since the new best is lower
+// (or higher) than everything else in the window and it is the most recent.
+// The window thus effectively gets reset on every new min. The same property
+// holds true for second best and third best estimates. Specifically, when a
+// sample arrives that is better than the second best but not better than the
+// best, it replaces the second and third best estimates but not the best
+// estimate. Similarly, a sample that is better than the third best estimate
+// but not the other estimates replaces only the third best estimate.
+//
+// Finally, when the best expires, it is replaced by the second best, which in
+// turn is replaced by the third best. The newest sample replaces the third
+// best.
+
+type windowedFilter struct {
+	// Time length of window
+	windowLength time.Duration
+	// Best estimate is element 0.
+	estimates [3]sample
+
+	compare func(a, b protocol.Bandwidth) bool
+}
+
+func newWindowedFilter(windowLength time.Duration) windowedFilter {
+	return windowedFilter{
+		windowLength: windowLength,
+		compare: func(a, b protocol.Bandwidth) bool { // this results in a max filter
+			return a >= b
+		},
+	}
+}
+
+// Update updates best estimates with |sample|, and expires and updates best
+// estimates as necessary.
+func (w *windowedFilter) Update(newBandwidth protocol.Bandwidth, newTime time.Time) {
+	// Reset all estimates if they have not yet been initialized, if new sample
+	// is a new best, or if the newest recorded estimate is too old.
+	if w.estimates[0].bandwidth == 0 || w.compare(newBandwidth, w.estimates[0].bandwidth) || newTime.Sub(w.estimates[2].time) > w.windowLength {
+		w.Reset(newBandwidth, newTime)
+		return
+	}
+
+	newSample := sample{
+		bandwidth: newBandwidth,
+		time:      newTime,
+	}
+
+	if w.compare(newBandwidth, w.estimates[1].bandwidth) {
+		w.estimates[1] = newSample
+		w.estimates[2] = w.estimates[1]
+	} else if w.compare(newBandwidth, w.estimates[2].bandwidth) {
+		w.estimates[2] = newSample
+	}
+
+	// Expire and update estimates as necessary.
+	if newTime.Sub(w.estimates[0].time) > w.windowLength {
+		// The best estimate hasn't been updated for an entire window, so promote
+		// second and third best estimates.
+		w.estimates[0] = w.estimates[1]
+		w.estimates[1] = w.estimates[2]
+		w.estimates[2] = newSample
+		// Need to iterate one more time. Check if the new best estimate is
+		// outside the window as well, since it may also have been recorded a
+		// long time ago. Don't need to iterate once more since we cover that
+		// case at the beginning of the method.
+		if newTime.Sub(w.estimates[0].time) > w.windowLength {
+			w.estimates[0] = w.estimates[1]
+			w.estimates[1] = w.estimates[2]
+		}
+		return
+	}
+
+	if w.estimates[1].bandwidth == w.estimates[0].bandwidth && newTime.Sub(w.estimates[1].time) > (w.windowLength>>2) {
+		// A quarter of the window has passed without a better sample, so the
+		// second-best estimate is taken from the second quarter of the window.
+		w.estimates[2] = newSample
+		w.estimates[1] = newSample
+		return
+	}
+
+	if w.estimates[2].bandwidth == w.estimates[1].bandwidth && newTime.Sub(w.estimates[2].time) > (w.windowLength>>1) {
+		// We've passed a half of the window without a better estimate, so take
+		// a third-best estimate from the second half of the window.
+		w.estimates[2] = newSample
+	}
+}
+
+// Reset resets all estimates to new sample.
+func (w *windowedFilter) Reset(newBandwidth protocol.Bandwidth, newTime time.Time) {
+	newSample := sample{bandwidth: newBandwidth, time: newTime}
+	w.estimates[0] = newSample
+	w.estimates[1] = newSample
+	w.estimates[2] = newSample
+}
+
+func (w *windowedFilter) GetBest() protocol.Bandwidth {
+	return w.estimates[0].bandwidth
+}
+
+func (w *windowedFilter) GetSecondBest() protocol.Bandwidth {
+	return w.estimates[1].bandwidth
+}
+
+func (w *windowedFilter) GetThirdBest() protocol.Bandwidth {
+	return w.estimates[2].bandwidth
+}
---
+++ congestion/bbr/windowed_filter_test.go
@@ -0,0 +1,190 @@
+package bbr
+
+import (
+	"time"
+
+	"github.com/lucas-clemente/quic-go/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+// only the tests for bandwidths (for the max filter)
+var _ = Describe("Windowed Filter", func() {
+	var (
+		filter    windowedFilter
+		startTime time.Time
+	)
+
+	BeforeEach(func() {
+		filter = newWindowedFilter(99 * time.Millisecond)
+		startTime = time.Now()
+	})
+
+	// Sets up windowed_max_bw_ to have the following values:
+	// Best = 900 bps, recorded at 25ms
+	// Second best = 700 bps, recorded at 75ms
+	// Third best = 600 bps, recorded at 100ms
+	initialize := func() {
+		bwSample := protocol.BandwidthFromDelta(1000, time.Second)
+		now := startTime
+
+		for i := 0; i < 5; i++ {
+			filter.Update(bwSample, now)
+			now = now.Add(25 * time.Millisecond)
+			bwSample -= protocol.BandwidthFromDelta(100, time.Second)
+		}
+
+		Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(900, time.Second)))
+		Expect(filter.GetSecondBest()).To(Equal(protocol.BandwidthFromDelta(700, time.Second)))
+		Expect(filter.GetThirdBest()).To(Equal(protocol.BandwidthFromDelta(600, time.Second)))
+	}
+
+	// updates the filter with a lot of small values in order
+	// to ensure that it is not susceptible to noise.
+	updateWithIrrelevantSamples := func(maxValue protocol.Bandwidth, t time.Time) {
+		for i := 0; i < 1000; i++ {
+			filter.Update(protocol.Bandwidth(i)%maxValue, t)
+		}
+	}
+
+	It("has the correct uninitialized estimates", func() {
+		filter = newWindowedFilter(0)
+		Expect(filter.GetBest()).To(BeZero())
+		Expect(filter.GetSecondBest()).To(BeZero())
+		Expect(filter.GetThirdBest()).To(BeZero())
+	})
+
+	It("handles a monotonically decreasing max", func() {
+		now := startTime.Add(-time.Hour)
+		bwSample := protocol.BandwidthFromDelta(1000, time.Second) // 1000 bits per second
+		filter.Update(bwSample, now)
+		Expect(filter.GetBest()).To(Equal(bwSample))
+
+		// Gradually decrease the bw samples and ensure the windowed max bw starts decreasing
+		for i := 0; i < 6; i++ {
+			now = now.Add(25 * time.Millisecond)
+			bwSample -= protocol.BandwidthFromDelta(100, time.Second)
+			filter.Update(bwSample, now)
+			if i < 3 {
+				Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(1000, time.Second)))
+			} else if i == 3 {
+				Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(900, time.Second)))
+			} else if i < 6 {
+				Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(700, time.Second)))
+			}
+		}
+	})
+
+	It("changes the third best", func() {
+		initialize()
+		// BW sample higher than the third-choice max sets that, but nothing else.
+		bwSample := filter.GetThirdBest() + protocol.BandwidthFromDelta(50, time.Second)
+		// Latest sample was recorded at 100ms.
+		now := startTime.Add(101 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(protocol.BandwidthFromDelta(700, time.Second)))
+		Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(900, time.Second)))
+	})
+
+	It("changes the second best", func() {
+		initialize()
+		// BW sample higher than the second-choice max sets that and also
+		// the third-choice max.
+		bwSample := filter.GetSecondBest() + protocol.BandwidthFromDelta(50, time.Second)
+		// Latest sample was recorded at 100ms.
+		now := startTime.Add(101 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(bwSample))
+		Expect(filter.GetBest()).To(Equal(protocol.BandwidthFromDelta(900, time.Second)))
+	})
+
+	It("changes all values", func() {
+		initialize()
+		// BW sample higher than the first-choice max sets that and also
+		// the second and third-choice maxs
+		bwSample := filter.GetBest() + protocol.BandwidthFromDelta(50, time.Second)
+		// Latest sample was recorded at 100ms.
+		now := startTime.Add(101 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(bwSample))
+		Expect(filter.GetBest()).To(Equal(bwSample))
+	})
+
+	It("expires best", func() {
+		initialize()
+		oldThirdBest := filter.GetThirdBest()
+		oldSecondBest := filter.GetSecondBest()
+		bwSample := oldThirdBest - protocol.BandwidthFromDelta(50, time.Second)
+		// Best max sample was recorded at 25ms, so expiry time is 124ms.
+		now := startTime.Add(125 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(oldThirdBest))
+		Expect(filter.GetBest()).To(Equal(oldSecondBest))
+	})
+
+	It("expires second best", func() {
+		initialize()
+		oldThirdBest := filter.GetThirdBest()
+		bwSample := oldThirdBest - protocol.BandwidthFromDelta(50, time.Second)
+		// Second best max sample was recorded at 75ms, so expiry time is 174ms.
+		now := startTime.Add(175 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(bwSample))
+		Expect(filter.GetBest()).To(Equal(oldThirdBest))
+	})
+
+	It("expires all", func() {
+		initialize()
+		bwSample := filter.GetThirdBest() - protocol.BandwidthFromDelta(50, time.Second)
+		// Third best max sample was recorded at 100ms, so expiry time is 199ms.
+		now := startTime.Add(200 * time.Millisecond)
+		filter.Update(bwSample, now)
+		Expect(filter.GetThirdBest()).To(Equal(bwSample))
+		Expect(filter.GetSecondBest()).To(Equal(bwSample))
+		Expect(filter.GetBest()).To(Equal(bwSample))
+	})
+
+	// Test the windowed filter where the time used is an exact counter instead of a
+	// timestamp.  This is useful if, for example, the time is measured in round
+	// trips.
+	It("expires counter based max", func() {
+		// Create a window which starts at t = 0 and expires after two cycles.
+		filter = newWindowedFilter(2 * time.Second)
+		now := startTime
+
+		// Insert 50000 at t = 1.
+		best := protocol.Bandwidth(50000)
+		now = now.Add(time.Second)
+		filter.Update(best, now)
+		Expect(filter.GetBest()).To(Equal(best))
+		updateWithIrrelevantSamples(20, now)
+		Expect(filter.GetBest()).To(Equal(best))
+
+		// Insert 40000 at t = 2.  Nothing is expected to expire.
+		now = now.Add(time.Second)
+		filter.Update(40000, now)
+		Expect(filter.GetBest()).To(Equal(best))
+		updateWithIrrelevantSamples(20, now)
+		Expect(filter.GetBest()).To(Equal(best))
+
+		// Insert 30000 at t = 3.  Nothing is expected to expire yet.
+		now = now.Add(time.Second)
+		filter.Update(30000, now)
+		Expect(filter.GetBest()).To(Equal(best))
+		updateWithIrrelevantSamples(20, now)
+		Expect(filter.GetBest()).To(Equal(best))
+
+		// Insert 20000 at t = 4.  50000 at t = 1 expires, so 40000 becomes the new maximum.
+		now = now.Add(time.Second)
+		newBest := protocol.Bandwidth(40000)
+		filter.Update(20000, now)
+		Expect(filter.GetBest()).To(Equal(newBest))
+		updateWithIrrelevantSamples(20, now)
+		Expect(filter.GetBest()).To(Equal(newBest))
+	})
+})
