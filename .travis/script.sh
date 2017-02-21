7a5955f560e83e02d2f960080ac8edbb44c15d15
Marten Seemann <martenseemann@gmail.com>
2017-02-21 11:54:38 +0700

    run benchmark tests with integration tests on travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,9 +4,10 @@ set -e
 
 go get -t ./...
 if [ ${TESTMODE} == "unit" ]; then
-  ginkgo -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtests
+  ginkgo -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtests --skipMeasurements
 fi
 
 if [ ${TESTMODE} == "integration" ]; then
+  ginkgo --randomizeAllSpecs --randomizeSuites --trace --progress -focus "Benchmark"
   ginkgo -r --randomizeAllSpecs --randomizeSuites --trace --progress integrationtests
 fi
