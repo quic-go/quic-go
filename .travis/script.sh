bc1858c7eca1f1f502955f23a06bf97486cff81b
Marten Seemann <martenseemann@gmail.com>
2017-07-25 11:21:47 +0700

    speed up the CI builds by using the new benchmark test flags
    Reduce the number of samples and the amount of data transferred.

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,11 +9,11 @@ fi
 
 if [ ${TESTMODE} == "integration" ]; then
   # run benchmark tests
-  ginkgo --randomizeAllSpecs --randomizeSuites --trace --progress benchmark
+  ginkgo --randomizeAllSpecs --randomizeSuites --trace --progress benchmark -- -samples=1
   # run benchmark tests with the Go race detector
   # The Go race detector only works on amd64.
   if [ ${TRAVIS_GOARCH} == 'amd64' ]; then
-    ginkgo --race --randomizeAllSpecs --randomizeSuites --trace --progress benchmark
+    ginkgo --race --randomizeAllSpecs --randomizeSuites --trace --progress benchmark -- -samples=1 -size=10
   fi
   # run integration tests
   ginkgo -v -r --randomizeAllSpecs --randomizeSuites --trace --progress integrationtests
