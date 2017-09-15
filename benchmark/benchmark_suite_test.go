322947f0a44929ccbc07311a247db4a6116c3b94
Marten Seemann <martenseemann@gmail.com>
2017-09-15 17:43:32 +0700

    WIP use netem to simulate different network conditions

--- benchmark/benchmark_suite_test.go
+++ benchmark/benchmark_suite_test.go
@@ -2,13 +2,20 @@ package benchmark
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
 
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/olekukonko/tablewriter"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
-	"github.com/onsi/ginkgo/reporters"
-	"github.com/onsi/ginkgo/reporters/stenographer"
 	"github.com/onsi/ginkgo/types"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 
 	"testing"
 )
@@ -17,14 +24,22 @@ var (
 	size    int // file size in MB, will be read from flags
 	samples int // number of samples for Measure, will be read from flags
 
+	netemAvailable         bool
 	averageTCPTransferRate float64
 )
 
-const tcpMeasurementLabel = "TCP transfer rate [MB/s]"
+const (
+	device = "dev lo" // the network device the netem rules will be applied to
+	// set test and context labels, so that our custom Ginkgo reporter can find them
+	tcpContextLabel   = "with TLS/TCP"
+	quicContextLabel  = "with QUIC %d"
+	transferRateLabel = "transfer rate [MB/s]"
+)
+
+var reporter = &myReporter{}
 
 func TestBenchmark(t *testing.T) {
 	RegisterFailHandler(Fail)
-	reporter := &myReporter{}
 	RunSpecsWithDefaultAndCustomReporters(t, "Benchmark Suite", []Reporter{reporter})
 }
 
@@ -32,17 +47,74 @@ func init() {
 	flag.IntVar(&size, "size", 50, "data length (in MB)")
 	flag.IntVar(&samples, "samples", 6, "number of samples")
 	flag.Parse()
+
+	_, err := exec.LookPath("tc")
+	netemAvailable = err == nil
+	fmt.Println("netemAvaliable", netemAvailable)
 }
+
+func clearNetem() {
+	if netemAvailable {
+		status := execNetem("tc qdisc show #device")
+		if strings.Contains(status, "netem") {
+			execNetem("tc qdisc del #device root")
+		}
+	}
+}
+
+func execNetem(cmd string) string {
+	if len(cmd) == 0 {
+		return ""
+	}
+	r := strings.NewReplacer("#device", "dev lo")
+	cmd = r.Replace(cmd)
+	w := strings.Split(cmd, " ")
+	command := exec.Command(w[0], w[1:]...)
+	command.SysProcAttr = &syscall.SysProcAttr{}
+	command.SysProcAttr.Credential = &syscall.Credential{Uid: 0, Gid: 0}
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).ToNot(HaveOccurred())
+	Eventually(session).Should(gexec.Exit(0))
+	return string(session.Out.Contents())
+}
+
+type networkCondition struct {
+	Description string
+	Command     string
+}
+
+var conditions = []networkCondition{
+	{Description: "direct transfer"},
+	{Description: "10ms RTT", Command: "tc qdisc add #device root netem delay 5ms"},
+	{Description: "50ms RTT", Command: "tc qdisc add #device root netem delay 25ms"},
+	{Description: "100ms RTT", Command: "tc qdisc add #device root netem delay 50ms"},
+	{Description: "400ms RTT", Command: "tc qdisc add #device root netem delay 200ms"},
+	{Description: "10ms ± 1ms RTT", Command: "tc qdisc add #device root netem delay 5ms 1ms"},
+	{Description: "50ms ± 5ms RTT", Command: "tc qdisc add #device root netem delay 25ms 5ms"},
+	{Description: "10ms RTT, 1% packet loss", Command: "tc qdisc add #device root netem delay 5ms drop 1%"},
+	{Description: "10ms RTT, 5% packet loss", Command: "tc qdisc add #device root netem delay 5ms drop 5%"},
+	{Description: "100ms RTT, 1% packet loss", Command: "tc qdisc add #device root netem delay 50ms drop 1%"},
+	{Description: "100ms RTT, 5% packet loss", Command: "tc qdisc add #device root netem delay 50ms drop 5%"},
+}
+
+type measurementSeries map[string]float64
+
+var _ = BeforeSuite(func() {
+	clearNetem()
+})
+
+var _ = AfterSuite(func() {
+	clearNetem()
+	reporter.printResult()
+})
 
 type myReporter struct {
 	Reporter
+
+	results map[string]measurementSeries
 }
 
 var _ Reporter = &myReporter{}
-
-func newReporter() Reporter {
-	return &myReporter{reporters.NewDefaultReporter(config.DefaultReporterConfig, stenographer.New(true, true))}
-}
 
 func (r *myReporter) SpecSuiteWillBegin(config.GinkgoConfigType, *types.SuiteSummary) {}
 func (r *myReporter) BeforeSuiteDidRun(*types.SetupSummary)                           {}
@@ -54,9 +126,57 @@ func (r *myReporter) SpecDidComplete(specSummary *types.SpecSummary) {
 	if !specSummary.IsMeasurement {
 		return
 	}
-	transferRate, ok := specSummary.Measurements[tcpMeasurementLabel]
+	cond := specSummary.ComponentTexts[2]
+	ver := specSummary.ComponentTexts[3]
+	transferRate, ok := specSummary.Measurements[transferRateLabel]
 	if !ok {
 		return
 	}
-	averageTCPTransferRate = transferRate.Average
+	r.addResult(cond, ver, transferRate.Average)
+	if ver == tcpContextLabel {
+		averageTCPTransferRate = transferRate.Average
+	}
+}
+
+func (r *myReporter) addResult(cond, ver string, transferRate float64) {
+	if r.results == nil {
+		r.results = make(map[string]measurementSeries)
+	}
+	if _, ok := r.results[cond]; !ok {
+		r.results[cond] = make(measurementSeries)
+	}
+	r.results[cond][ver] = transferRate
+}
+
+func (r *myReporter) printResult() {
+	table := tablewriter.NewWriter(os.Stdout)
+	header := []string{"", "TCP"}
+	for _, v := range protocol.SupportedVersions {
+		header = append(header, "QUIC "+strconv.Itoa(int(v)))
+	}
+	table.SetHeader(header)
+	table.SetCaption(true, fmt.Sprintf("Based on %d samples of %d MB.\nAll values in MB/s.", samples, size))
+
+	for _, cond := range conditions {
+		data := make([]string, len(protocol.SupportedVersions)+2)
+		data[0] = cond.Description
+		tcpRate, ok := r.results[cond.Description][tcpContextLabel]
+		if !ok {
+			data[1] = "-"
+		} else {
+			data[1] = fmt.Sprintf("%.2f", tcpRate)
+		}
+		for i, ver := range protocol.SupportedVersions {
+			val, ok := r.results[cond.Description][fmt.Sprintf(quicContextLabel, ver)]
+			var out string
+			if !ok {
+				out = "-"
+			} else {
+				out = fmt.Sprintf("%.2f", val) + " (" + fmt.Sprintf("%.1f", 100*val/tcpRate) + "%)"
+			}
+			data[i+2] = out
+		}
+		table.Append(data)
+	}
+	table.Render()
 }
--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -23,98 +23,121 @@ func init() {
 		rand.Seed(GinkgoRandomSeed())
 		rand.Read(data) // no need to check for an error. math.Rand.Read never errors
 
-		Measure(fmt.Sprintf("transferring a %d MB file", size), func(b Benchmarker) {
-			serverAddr := make(chan *net.TCPAddr)
-			go func() {
-				defer GinkgoRecover()
-				ln, err := tls.Listen(
-					"tcp",
-					"127.0.0.1:0",
-					testdata.GetTLSConfig(),
-				)
-				serverAddr <- ln.Addr().(*net.TCPAddr)
-				sess, err := ln.Accept()
-				Expect(err).ToNot(HaveOccurred())
-				_, err = sess.Write(data)
-				Expect(err).ToNot(HaveOccurred())
-				err = sess.Close()
-				Expect(err).ToNot(HaveOccurred())
-			}()
+		for _, c := range conditions {
+			cond := c
+			Context(cond.Description, func() {
+				BeforeEach(func() {
+					if len(cond.Command) > 0 {
+						if !netemAvailable {
+							Skip("Skipping. netem not found.")
+						}
+						execNetem(cond.Command)
+					}
+				})
 
-			addr := <-serverAddr
-			conn, err := tls.Dial("tcp", addr.String(), &tls.Config{InsecureSkipVerify: true})
-			Expect(err).ToNot(HaveOccurred())
+				AfterEach(func() {
+					// TODO: make sure this is always executed
+					if len(cond.Command) > 0 {
+						execNetem("tc qdisc del dev lo root")
+					}
+				})
 
-			buf := &bytes.Buffer{}
-			// measure the time it takes to download the dataLen bytes
-			runtime := b.Time("transfer time", func() {
-				_, err := io.Copy(buf, conn)
-				Expect(err).NotTo(HaveOccurred())
-			})
-			Expect(buf.Bytes()).To(Equal(data))
-			b.RecordValue(tcpMeasurementLabel, float64(dataLen)/(1<<20)/runtime.Seconds())
-		}, samples)
+				Context(tcpContextLabel, func() {
+					Measure(fmt.Sprintf("transferring a %d MB file", size), func(b Benchmarker) {
+						serverAddr := make(chan *net.TCPAddr)
+						go func() {
+							defer GinkgoRecover()
+							ln, err := tls.Listen(
+								"tcp",
+								"127.0.0.1:0",
+								testdata.GetTLSConfig(),
+							)
+							serverAddr <- ln.Addr().(*net.TCPAddr)
+							sess, err := ln.Accept()
+							Expect(err).ToNot(HaveOccurred())
+							_, err = sess.Write(data)
+							Expect(err).ToNot(HaveOccurred())
+							err = sess.Close()
+							Expect(err).ToNot(HaveOccurred())
+						}()
 
-		for i := range protocol.SupportedVersions {
-			version := protocol.SupportedVersions[i]
-
-			Context(fmt.Sprintf("with version %s", version), func() {
-				Measure(fmt.Sprintf("transferring a %d MB file", size), func(b Benchmarker) {
-					var ln quic.Listener
-					serverAddr := make(chan net.Addr)
-					handshakeChan := make(chan struct{})
-					// start the server
-					go func() {
-						defer GinkgoRecover()
-						var err error
-						ln, err = quic.ListenAddr(
-							"localhost:0",
-							testdata.GetTLSConfig(),
-							&quic.Config{Versions: []protocol.VersionNumber{version}},
-						)
+						addr := <-serverAddr
+						conn, err := tls.Dial("tcp", addr.String(), &tls.Config{InsecureSkipVerify: true})
 						Expect(err).ToNot(HaveOccurred())
-						serverAddr <- ln.Addr()
-						sess, err := ln.Accept()
-						Expect(err).ToNot(HaveOccurred())
-						// wait for the client to complete the handshake before sending the data
-						// this should not be necessary, but due to timing issues on the CIs, this is necessary to avoid sending too many undecryptable packets
-						<-handshakeChan
-						str, err := sess.OpenStream()
-						Expect(err).ToNot(HaveOccurred())
-						_, err = str.Write(data)
-						Expect(err).ToNot(HaveOccurred())
-						err = str.Close()
-						Expect(err).ToNot(HaveOccurred())
-					}()
 
-					// start the client
-					addr := <-serverAddr
-					sess, err := quic.DialAddr(
-						addr.String(),
-						&tls.Config{InsecureSkipVerify: true},
-						&quic.Config{Versions: []protocol.VersionNumber{version}},
-					)
-					Expect(err).ToNot(HaveOccurred())
-					close(handshakeChan)
-					str, err := sess.AcceptStream()
-					Expect(err).ToNot(HaveOccurred())
+						buf := &bytes.Buffer{}
+						// measure the time it takes to download the dataLen bytes
+						runtime := b.Time("transfer time", func() {
+							_, err := io.Copy(buf, conn)
+							Expect(err).NotTo(HaveOccurred())
+						})
+						Expect(buf.Bytes()).To(Equal(data))
+						b.RecordValue(transferRateLabel, float64(dataLen)/(1<<20)/runtime.Seconds())
+					}, 5*samples)
+				})
 
-					buf := &bytes.Buffer{}
-					// measure the time it takes to download the dataLen bytes
-					// note we're measuring the time for the transfer, i.e. excluding the handshake
-					runtime := b.Time("transfer time", func() {
-						_, err := io.Copy(buf, str)
-						Expect(err).NotTo(HaveOccurred())
+				for i := range protocol.SupportedVersions {
+					version := protocol.SupportedVersions[i]
+
+					Context(fmt.Sprintf(quicContextLabel, version), func() {
+						Measure(fmt.Sprintf("transferring a %d MB file", size), func(b Benchmarker) {
+							var ln quic.Listener
+							serverAddr := make(chan net.Addr)
+							handshakeChan := make(chan struct{})
+							// start the server
+							go func() {
+								defer GinkgoRecover()
+								var err error
+								ln, err = quic.ListenAddr(
+									"localhost:0",
+									testdata.GetTLSConfig(),
+									&quic.Config{Versions: []protocol.VersionNumber{version}},
+								)
+								Expect(err).ToNot(HaveOccurred())
+								serverAddr <- ln.Addr()
+								sess, err := ln.Accept()
+								Expect(err).ToNot(HaveOccurred())
+								// wait for the client to complete the handshake before sending the data
+								// this should not be necessary, but due to timing issues on the CIs, this is necessary to avoid sending too many undecryptable packets
+								<-handshakeChan
+								str, err := sess.OpenStream()
+								Expect(err).ToNot(HaveOccurred())
+								_, err = str.Write(data)
+								Expect(err).ToNot(HaveOccurred())
+								err = str.Close()
+								Expect(err).ToNot(HaveOccurred())
+							}()
+
+							// start the client
+							addr := <-serverAddr
+							sess, err := quic.DialAddr(
+								addr.String(),
+								&tls.Config{InsecureSkipVerify: true},
+								&quic.Config{Versions: []protocol.VersionNumber{version}},
+							)
+							Expect(err).ToNot(HaveOccurred())
+							close(handshakeChan)
+							str, err := sess.AcceptStream()
+							Expect(err).ToNot(HaveOccurred())
+
+							buf := &bytes.Buffer{}
+							// measure the time it takes to download the dataLen bytes
+							// note we're measuring the time for the transfer, i.e. excluding the handshake
+							runtime := b.Time("transfer time", func() {
+								_, err := io.Copy(buf, str)
+								Expect(err).NotTo(HaveOccurred())
+							})
+							Expect(buf.Bytes()).To(Equal(data))
+
+							transferRate := float64(dataLen) / 1e6 / runtime.Seconds()
+							b.RecordValue(transferRateLabel, transferRate)
+							b.RecordValue("comparison to TCP [%]", 100*transferRate/averageTCPTransferRate)
+
+							ln.Close()
+							sess.Close(nil)
+						}, samples)
 					})
-					Expect(buf.Bytes()).To(Equal(data))
-
-					transferRate := float64(dataLen) / 1e6 / runtime.Seconds()
-					b.RecordValue("transfer rate [MB/s]", transferRate)
-					b.RecordValue("comparison to TCP [%]", 100*transferRate/averageTCPTransferRate)
-
-					ln.Close()
-					sess.Close(nil)
-				}, samples)
+				}
 			})
 		}
 	})
