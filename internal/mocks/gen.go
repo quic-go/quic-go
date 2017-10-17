6dc453caa3719685b3b133ccffeec4311230c5a9
Marten Seemann <martenseemann@gmail.com>
2017-10-17 21:47:35 +0700

    use mockgen reflect mode
    This requires a dirty hack to get mockgen to generate mocks for internal
    packages.

--- internal/mocks/gen.go
+++ internal/mocks/gen.go
@@ -1,7 +1,4 @@
 package mocks
 
-// mockgen source mode doesn't properly recognize structs defined in the same package
-// so we have to use sed to correct for that
-
-//go:generate sh -c "mockgen -package mocks -source ../flowcontrol/interface.go | sed \"s/\\[\\]WindowUpdate/[]flowcontrol.WindowUpdate/g\" > flow_control_manager.go"
+//go:generate sh -c "./mockgen_internal.sh mocks flow_control_manager.go github.com/lucas-clemente/quic-go/internal/flowcontrol FlowControlManager"
 //go:generate sh -c "goimports -w ."
