615dfddfa958d3bdf54f61769743196f4d093866
Julien Schmidt <git@julienschmidt.com>
2017-12-12 20:29:22 +0100

    revert ByteCount alias change and cast instead

--- stream.go
+++ stream.go
@@ -387,7 +387,7 @@ func (s *stream) SetDeadline(t time.Time) error {
 
 // CloseRemote makes the stream receive a "virtual" FIN stream frame at a given offset
 func (s *stream) CloseRemote(offset uint64) {
-	s.AddStreamFrame(&wire.StreamFrame{FinBit: true, Offset: offset})
+	s.AddStreamFrame(&wire.StreamFrame{FinBit: true, Offset: protocol.ByteCount(offset)})
 }
 
 // Cancel is called by session to indicate that an error occurred
