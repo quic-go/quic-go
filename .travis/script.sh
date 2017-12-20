5bd898323876f20ad48e755d8090d1f3a687f6fa
Marten Seemann <martenseemann@gmail.com>
2017-12-20 18:00:37 +0700

    temporarily disable running the benchmark tests with race detector

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,11 +10,12 @@ fi
 if [ ${TESTMODE} == "integration" ]; then
   # run benchmark tests
   ginkgo -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1
-  # run benchmark tests with the Go race detector
-  # The Go race detector only works on amd64.
-  if [ ${TRAVIS_GOARCH} == 'amd64' ]; then
-    ginkgo -race -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1 -size=10
-  fi
+  # TODO(#1055): reenable this test
+  # # run benchmark tests with the Go race detector
+  # # The Go race detector only works on amd64.
+  # if [ ${TRAVIS_GOARCH} == 'amd64' ]; then
+  #   ginkgo -race -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1 -size=10
+  # fi
   # run integration tests
   ginkgo -r -v -randomizeAllSpecs -randomizeSuites -trace integrationtests
 fi
