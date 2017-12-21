f2a6c251242e005a9f2aa0fb0faf7af8536591ba
Marten Seemann <martenseemann@gmail.com>
2017-12-21 13:38:13 +0700

    remove unneeded mockgen command

--- internal/mocks/gen.go
+++ internal/mocks/gen.go
@@ -7,5 +7,4 @@ package mocks
 //go:generate sh -c "./mockgen_internal.sh mocks received_packet_handler.go github.com/lucas-clemente/quic-go/ackhandler ReceivedPacketHandler"
 //go:generate sh -c "./mockgen_internal.sh mocks connection_flow_controller.go github.com/lucas-clemente/quic-go/internal/flowcontrol ConnectionFlowController"
 //go:generate sh -c "./mockgen_internal.sh mockcrypto crypto/aead.go github.com/lucas-clemente/quic-go/internal/crypto AEAD"
-//go:generate sh -c "./mockgen_stream.sh mocks stream.go github.com/lucas-clemente/quic-go StreamI"
 //go:generate sh -c "goimports -w ."
