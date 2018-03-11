0bdf99ca3f1a76e9f07a80d9a4003fd90b3b3a39
ferrieux <alexandre.ferrieux@orange.com>
2018-03-11 17:23:02 +0100

    Forgotten check-in of globals.go

---
+++ internal/utils/globals.go
@@ -0,0 +1,8 @@
+package utils
+
+type globals struct {
+	// HasMeasurementByte defines whether the 2nd byte in short headers is the measurement byte
+	HasMeasurementByte bool
+}
+
+var Globals globals
