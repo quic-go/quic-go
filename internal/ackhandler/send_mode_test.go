f0528617751aa7b813b275dfdf2226526c60de87
Marten Seemann <martenseemann@gmail.com>
2018-03-16 18:36:37 +0000

    only allow sending of retransmissions if these are RTO probe packets

--- internal/ackhandler/send_mode_test.go
+++ internal/ackhandler/send_mode_test.go
@@ -10,6 +10,7 @@ var _ = Describe("Send Mode", func() {
 		Expect(SendNone.String()).To(Equal("none"))
 		Expect(SendAny.String()).To(Equal("any"))
 		Expect(SendAck.String()).To(Equal("ack"))
+		Expect(SendRTO.String()).To(Equal("rto"))
 		Expect(SendRetransmission.String()).To(Equal("retransmission"))
 		Expect(SendMode(123).String()).To(Equal("invalid send mode: 123"))
 	})
