a2fd6e832869e63d2115186dbdfd7ffea6a7f0b0
ferrieux <alexandre.ferrieux@orange.com>
2018-04-04 12:03:59 +0200

    Disable gQUIC compat for now

--- internal/wire/header.go
+++ internal/wire/header.go
@@ -80,7 +80,7 @@ func ParseHeaderSentByClient(b *bytes.Reader) (*Header, error) {
 
 func parsePacketHeader(b *bytes.Reader, sentBy protocol.Perspective, isPublicHeader bool) (*Header, error) {
 	// This is a gQUIC Public Header.
-	if isPublicHeader {
+	if false && isPublicHeader { // Disable gQUIC compat for now
 		hdr, err := parsePublicHeader(b, sentBy)
 		if err != nil {
 			return nil, err
