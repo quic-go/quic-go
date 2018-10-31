73ac6b14bf9e5169af419005992b35ea85ae9a3f
Marten Seemann <martenseemann@gmail.com>
2018-10-31 11:21:51 +0700

    implement changes to the CONNECTION_CLOSE frame

--- internal/wire/connection_close_frame.go
+++ internal/wire/connection_close_frame.go
@@ -2,39 +2,44 @@ package wire
 
 import (
 	"bytes"
-	"errors"
 	"io"
-	"math"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/qerr"
 )
 
-// A ConnectionCloseFrame in QUIC
+// A ConnectionCloseFrame is a CONNECTION_CLOSE frame
 type ConnectionCloseFrame struct {
-	ErrorCode    qerr.ErrorCode
-	ReasonPhrase string
+	IsApplicationError bool
+	ErrorCode          qerr.ErrorCode
+	ReasonPhrase       string
 }
 
 // parseConnectionCloseFrame reads a CONNECTION_CLOSE frame
 func parseConnectionCloseFrame(r *bytes.Reader, version protocol.VersionNumber) (*ConnectionCloseFrame, error) {
-	if _, err := r.ReadByte(); err != nil { // read the TypeByte
+	typeByte, err := r.ReadByte()
+	if err != nil {
 		return nil, err
 	}
 
-	var errorCode qerr.ErrorCode
-	var reasonPhraseLen uint64
+	f := &ConnectionCloseFrame{IsApplicationError: typeByte == 0x03}
 	ec, err := utils.BigEndian.ReadUint16(r)
 	if err != nil {
 		return nil, err
 	}
-	errorCode = qerr.ErrorCode(ec)
+	f.ErrorCode = qerr.ErrorCode(ec)
+	// read the Frame Type, if this is not an application error
+	if !f.IsApplicationError {
+		if _, err := utils.ReadVarInt(r); err != nil {
+			return nil, err
+		}
+	}
+	var reasonPhraseLen uint64
 	reasonPhraseLen, err = utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
-
 	// shortcut to prevent the unnecessary allocation of dataLen bytes
 	// if the dataLen is larger than the remaining length of the packet
 	// reading the whole reason phrase would result in EOF when attempting to READ
@@ -47,29 +52,32 @@ func parseConnectionCloseFrame(r *bytes.Reader, version protocol.VersionNumber) 
 		// this should never happen, since we already checked the reasonPhraseLen earlier
 		return nil, err
 	}
-
-	return &ConnectionCloseFrame{
-		ErrorCode:    errorCode,
-		ReasonPhrase: string(reasonPhrase),
-	}, nil
+	f.ReasonPhrase = string(reasonPhrase)
+	return f, nil
 }
 
 // Length of a written frame
 func (f *ConnectionCloseFrame) Length(version protocol.VersionNumber) protocol.ByteCount {
-	return 1 + 2 + utils.VarIntLen(uint64(len(f.ReasonPhrase))) + protocol.ByteCount(len(f.ReasonPhrase))
+	length := 1 + 2 + utils.VarIntLen(uint64(len(f.ReasonPhrase))) + protocol.ByteCount(len(f.ReasonPhrase))
+	if !f.IsApplicationError {
+		length++ // for the frame type
+	}
+	return length
 }
 
 // Write writes an CONNECTION_CLOSE frame.
 func (f *ConnectionCloseFrame) Write(b *bytes.Buffer, version protocol.VersionNumber) error {
-	b.WriteByte(0x02)
-
-	if len(f.ReasonPhrase) > math.MaxUint16 {
-		return errors.New("ConnectionFrame: ReasonPhrase too long")
+	if f.IsApplicationError {
+		b.WriteByte(0x03)
+	} else {
+		b.WriteByte(0x02)
 	}
 
 	utils.BigEndian.WriteUint16(b, uint16(f.ErrorCode))
+	if !f.IsApplicationError {
+		utils.WriteVarInt(b, 0)
+	}
 	utils.WriteVarInt(b, uint64(len(f.ReasonPhrase)))
 	b.WriteString(f.ReasonPhrase)
-
 	return nil
 }
