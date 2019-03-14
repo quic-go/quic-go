ec70e6afc89ccda084d5b5f8f86ef0b6f01eeca5
Marten Seemann <martenseemann@gmail.com>
2019-03-14 13:41:15 +0900

    disable linters on Travis
    Gometalinter stopped working recently.

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,11 +3,6 @@
 set -ex
 
 go get -t ./...
-if [ ${TESTMODE} == "lint" ]; then 
-  go get github.com/alecthomas/gometalinter
-  gometalinter --install
-  gometalinter --deadline=300s --tests ./...
-fi
 
 if [ ${TESTMODE} == "unit" ]; then
   ginkgo -r -v -cover -randomizeAllSpecs -randomizeSuites -trace -skipPackage integrationtests,benchmark
