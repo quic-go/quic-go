b1fc984306b803574d0edb64e0ec566147eb0b60
Marten Seemann <martenseemann@gmail.com>
2019-06-23 13:37:26 +0800

    add a 0-RTT encryption level

--- internal/protocol/encryption_level_test.go
+++ internal/protocol/encryption_level_test.go
@@ -10,6 +10,7 @@ var _ = Describe("Encryption Level", func() {
 		Expect(EncryptionUnspecified.String()).To(Equal("unknown"))
 		Expect(EncryptionInitial.String()).To(Equal("Initial"))
 		Expect(EncryptionHandshake.String()).To(Equal("Handshake"))
+		Expect(Encryption0RTT.String()).To(Equal("0-RTT"))
 		Expect(Encryption1RTT.String()).To(Equal("1-RTT"))
 	})
 })
