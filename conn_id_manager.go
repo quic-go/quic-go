031c1709e6b18c0db1cca7c0a5518f9862913426
Marten Seemann <martenseemann@gmail.com>
2019-09-14 14:11:28 +0700

    implement a basic store for new connection IDs

---
+++ conn_id_manager.go
@@ -0,0 +1,45 @@
+package quic
+
+import (
+	"fmt"
+
+	"github.com/lucas-clemente/quic-go/internal/utils"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+)
+
+type connIDManager struct {
+	queue utils.NewConnectionIDList
+}
+
+func (h *connIDManager) Add(f *wire.NewConnectionIDFrame) error {
+	// insert a new element at the end
+	if h.queue.Len() == 0 || h.queue.Back().Value.SequenceNumber < f.SequenceNumber {
+		h.queue.PushBack(utils.NewConnectionID{
+			SequenceNumber:      f.SequenceNumber,
+			ConnectionID:        f.ConnectionID,
+			StatelessResetToken: &f.StatelessResetToken,
+		})
+		return nil
+	}
+	// insert a new element somewhere in the middle
+	for el := h.queue.Front(); el != nil; el = el.Next() {
+		if el.Value.SequenceNumber == f.SequenceNumber {
+			if !el.Value.ConnectionID.Equal(f.ConnectionID) {
+				return fmt.Errorf("received conflicting connection IDs for sequence number %d", f.SequenceNumber)
+			}
+			if *el.Value.StatelessResetToken != f.StatelessResetToken {
+				return fmt.Errorf("received conflicting stateless reset tokens for sequence number %d", f.SequenceNumber)
+			}
+			return nil
+		}
+		if el.Value.SequenceNumber > f.SequenceNumber {
+			h.queue.InsertBefore(utils.NewConnectionID{
+				SequenceNumber:      f.SequenceNumber,
+				ConnectionID:        f.ConnectionID,
+				StatelessResetToken: &f.StatelessResetToken,
+			}, el)
+			return nil
+		}
+	}
+	panic("should have processed NEW_CONNECTION_ID frame")
+}
---
+++ conn_id_manager_test.go
@@ -0,0 +1,92 @@
+package quic
+
+import (
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Connection ID Manager", func() {
+	var m *connIDManager
+
+	BeforeEach(func() {
+		m = &connIDManager{}
+	})
+
+	get := func() (protocol.ConnectionID, *[16]byte) {
+		if m.queue.Len() == 0 {
+			return nil, nil
+		}
+		val := m.queue.Remove(m.queue.Front())
+		return val.ConnectionID, val.StatelessResetToken
+	}
+
+	It("returns nil if empty", func() {
+		c, rt := get()
+		Expect(c).To(BeNil())
+		Expect(rt).To(BeNil())
+	})
+
+	It("adds and gets connection IDs", func() {
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber:      10,
+			ConnectionID:        protocol.ConnectionID{2, 3, 4, 5},
+			StatelessResetToken: [16]byte{0xe, 0xd, 0xc, 0xb, 0xa, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+		})).To(Succeed())
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber:      4,
+			ConnectionID:        protocol.ConnectionID{1, 2, 3, 4},
+			StatelessResetToken: [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe},
+		})).To(Succeed())
+		c1, rt1 := get()
+		Expect(c1).To(Equal(protocol.ConnectionID{1, 2, 3, 4}))
+		Expect(*rt1).To(Equal([16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe}))
+		c2, rt2 := get()
+		Expect(c2).To(Equal(protocol.ConnectionID{2, 3, 4, 5}))
+		Expect(*rt2).To(Equal([16]byte{0xe, 0xd, 0xc, 0xb, 0xa, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}))
+		c3, rt3 := get()
+		Expect(c3).To(BeNil())
+		Expect(rt3).To(BeNil())
+	})
+
+	It("accepts duplicates", func() {
+		f := &wire.NewConnectionIDFrame{
+			SequenceNumber:      1,
+			ConnectionID:        protocol.ConnectionID{1, 2, 3, 4},
+			StatelessResetToken: [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe},
+		}
+		Expect(m.Add(f)).To(Succeed())
+		Expect(m.Add(f)).To(Succeed())
+		c1, rt1 := get()
+		Expect(c1).To(Equal(protocol.ConnectionID{1, 2, 3, 4}))
+		Expect(*rt1).To(Equal([16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe}))
+		c2, rt2 := get()
+		Expect(c2).To(BeNil())
+		Expect(rt2).To(BeNil())
+	})
+
+	It("rejects duplicates with different connection IDs", func() {
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber: 42,
+			ConnectionID:   protocol.ConnectionID{1, 2, 3, 4},
+		})).To(Succeed())
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber: 42,
+			ConnectionID:   protocol.ConnectionID{2, 3, 4, 5},
+		})).To(MatchError("received conflicting connection IDs for sequence number 42"))
+	})
+
+	It("rejects duplicates with different connection IDs", func() {
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber:      42,
+			ConnectionID:        protocol.ConnectionID{1, 2, 3, 4},
+			StatelessResetToken: [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe},
+		})).To(Succeed())
+		Expect(m.Add(&wire.NewConnectionIDFrame{
+			SequenceNumber:      42,
+			ConnectionID:        protocol.ConnectionID{1, 2, 3, 4},
+			StatelessResetToken: [16]byte{0xe, 0xd, 0xc, 0xb, 0xa, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+		})).To(MatchError("received conflicting stateless reset tokens for sequence number 42"))
+	})
+})
