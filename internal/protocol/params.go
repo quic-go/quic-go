bd94f21ab091e4e3403869faa43605db457d5e0d
Marten Seemann <martenseemann@gmail.com>
2019-11-10 11:06:38 +0700

    send keep alive PINGs at least every 20 seconds

--- internal/protocol/params.go
+++ internal/protocol/params.go
@@ -99,6 +99,10 @@ const DefaultIdleTimeout = 30 * time.Second
 // DefaultHandshakeTimeout is the default timeout for a connection until the crypto handshake succeeds.
 const DefaultHandshakeTimeout = 10 * time.Second
 
+// MaxKeepAliveInterval is the maximum time until we send a packet to keep a connection alive.
+// It should be shorter than the time that NATs clear their mapping.
+const MaxKeepAliveInterval = 20 * time.Second
+
 // RetiredConnectionIDDeleteTimeout is the time we keep closed sessions around in order to retransmit the CONNECTION_CLOSE.
 // after this time all information about the old connection will be deleted
 const RetiredConnectionIDDeleteTimeout = 5 * time.Second
