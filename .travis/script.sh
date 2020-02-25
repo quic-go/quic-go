b128ad50c9b52011ca852483bd804cfb8c4f4e18
Marten Seemann <martenseemann@gmail.com>
2020-02-25 15:08:25 +0700

    run the internal and http3 tests with race detector on Travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -13,6 +13,11 @@ fi
 
 if [ ${TESTMODE} == "unit" ]; then
   ginkgo -r -v -cover -randomizeAllSpecs -randomizeSuites -trace -skipPackage integrationtests,benchmark
+  # run internal and http3 tests with the Go race detector
+  # The Go race detector only works on amd64.
+  if [ ${TRAVIS_GOARCH} == 'amd64' ]; then
+    ginkgo -r -v -race -randomizeAllSpecs -randomizeSuites -trace internal http3
+  fi
 fi
 
 if [ ${TESTMODE} == "integration" ]; then
