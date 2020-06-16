312b8d37f077dedcb41db2a536a7050244b37e7e
Marten Seemann <martenseemann@gmail.com>
2020-06-16 14:01:11 +0700

    use a callback to pass the bandwidth to the pacer

--- internal/congestion/pacer.go
+++ internal/congestion/pacer.go
@@ -14,11 +14,14 @@ const maxBurstSize = 10 * maxDatagramSize
 type pacer struct {
 	budgetAtLastSent protocol.ByteCount
 	lastSentTime     time.Time
-	bandwidth        uint64 // in bytes / s
+	getBandwidth     func() uint64 // in bytes/s
 }
 
-func newPacer(bw uint64) *pacer {
-	p := &pacer{bandwidth: bw}
+func newPacer(getBandwidth func() Bandwidth) *pacer {
+	p := &pacer{getBandwidth: func() uint64 {
+		// Bandwidth is in bits/s. We need the value in bytes/s.
+		return uint64(getBandwidth() / BytesPerSecond)
+	}}
 	p.budgetAtLastSent = p.maxBurstSize()
 	return p
 }
@@ -33,24 +36,17 @@ func (p *pacer) SentPacket(sendTime time.Time, size protocol.ByteCount) {
 	p.lastSentTime = sendTime
 }
 
-func (p *pacer) SetBandwidth(bw uint64) {
-	if bw == 0 {
-		panic("zero bandwidth")
-	}
-	p.bandwidth = bw
-}
-
 func (p *pacer) Budget(now time.Time) protocol.ByteCount {
 	if p.lastSentTime.IsZero() {
 		return p.maxBurstSize()
 	}
-	budget := p.budgetAtLastSent + (protocol.ByteCount(p.bandwidth)*protocol.ByteCount(now.Sub(p.lastSentTime).Nanoseconds()))/1e9
+	budget := p.budgetAtLastSent + (protocol.ByteCount(p.getBandwidth())*protocol.ByteCount(now.Sub(p.lastSentTime).Nanoseconds()))/1e9
 	return utils.MinByteCount(p.maxBurstSize(), budget)
 }
 
 func (p *pacer) maxBurstSize() protocol.ByteCount {
 	return utils.MaxByteCount(
-		protocol.ByteCount(uint64((protocol.MinPacingDelay+protocol.TimerGranularity).Nanoseconds())*p.bandwidth)/1e9,
+		protocol.ByteCount(uint64((protocol.MinPacingDelay+protocol.TimerGranularity).Nanoseconds())*p.getBandwidth())/1e9,
 		maxBurstSize,
 	)
 }
@@ -62,6 +58,6 @@ func (p *pacer) TimeUntilSend() time.Time {
 	}
 	return p.lastSentTime.Add(utils.MaxDuration(
 		protocol.MinPacingDelay,
-		time.Duration(math.Ceil(float64(maxDatagramSize-p.budgetAtLastSent)*1e9/float64(p.bandwidth)))*time.Nanosecond,
+		time.Duration(math.Ceil(float64(maxDatagramSize-p.budgetAtLastSent)*1e9/float64(p.getBandwidth())))*time.Nanosecond,
 	))
 }
--- internal/congestion/pacer_test.go
+++ internal/congestion/pacer_test.go
@@ -13,9 +13,11 @@ var _ = Describe("Pacer", func() {
 	var p *pacer
 
 	const packetsPerSecond = 42
+	var bandwidth uint64 // in bytes/s
 
 	BeforeEach(func() {
-		p = newPacer(packetsPerSecond * uint64(maxDatagramSize)) // bandwidth: 42 full-size packets per second
+		bandwidth = uint64(packetsPerSecond * maxDatagramSize) // 42 full-size packets per second
+		p = newPacer(func() Bandwidth { return Bandwidth(bandwidth) * BytesPerSecond })
 	})
 
 	It("allows a burst at the beginning", func() {
@@ -26,7 +28,7 @@ var _ = Describe("Pacer", func() {
 
 	It("allows a big burst for high pacing rates", func() {
 		t := time.Now()
-		p.SetBandwidth(10000 * packetsPerSecond * uint64(maxDatagramSize))
+		bandwidth = uint64(10000 * packetsPerSecond * maxDatagramSize)
 		Expect(p.TimeUntilSend()).To(BeZero())
 		Expect(p.Budget(t)).To(BeNumerically(">", maxBurstSize))
 	})
@@ -94,14 +96,14 @@ var _ = Describe("Pacer", func() {
 	It("changes the bandwidth", func() {
 		t := time.Now()
 		sendBurst(t)
-		p.SetBandwidth(uint64(maxDatagramSize)) // reduce the bandwidth to 1 packet per second
+		bandwidth = uint64(maxDatagramSize) // reduce the bandwidth to 1 packet per second
 		Expect(p.TimeUntilSend()).To(Equal(t.Add(time.Second)))
 	})
 
 	It("doesn't pace faster than the minimum pacing duration", func() {
 		t := time.Now()
 		sendBurst(t)
-		p.SetBandwidth(1e6 * uint64(maxDatagramSize))
+		bandwidth = uint64(1e6 * maxDatagramSize)
 		Expect(p.TimeUntilSend()).To(Equal(t.Add(protocol.MinPacingDelay)))
 		Expect(p.Budget(t.Add(protocol.MinPacingDelay))).To(Equal(protocol.ByteCount(protocol.MinPacingDelay) * maxDatagramSize * 1e6 / 1e9))
 	})
