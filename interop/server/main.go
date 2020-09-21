960fd8b8825a769d30ccbab2d2eaf67c2dc86be7
Marten Seemann <martenseemann@gmail.com>
2020-09-21 12:06:11 +0700

    remove support for the ECN test case

--- interop/server/main.go
+++ interop/server/main.go
@@ -52,7 +52,7 @@ func main() {
 	tlsConf.KeyLogWriter = keyLog
 
 	switch testcase {
-	case "versionnegotiation", "handshake", "transfer", "resumption", "zerortt", "multiconnect", "ecn":
+	case "versionnegotiation", "handshake", "transfer", "resumption", "zerortt", "multiconnect":
 		err = runHTTP09Server(quicConf)
 	case "retry":
 		// By default, quic-go performs a Retry on every incoming connection.
