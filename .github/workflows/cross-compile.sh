cc619f118e2254bf9609150103d590fea052d937
Marten Seemann <martenseemann@gmail.com>
2020-12-30 13:38:43 +0800

    fix YAML syntax error in setup-go step

--- .github/workflows/cross-compile.sh
+++ .github/workflows/cross-compile.sh
@@ -10,11 +10,13 @@ for dist in $(go tool dist list); do
 	if [[ "$goos" == "android" ]]; then continue; fi 		# cross-compiling for android is a pain...
 	if [[ "$goos" == "darwin" && $goarch == "arm64" ]]; then continue; fi # ... darwin/arm64 neither
 	if [[ $GOVERSION == "1.14" && $goos == "darwin" && $goarch == "arm" ]]; then continue; fi # Go 1.14 lacks syscall.IPV6_RECVTCLASS
-
-	cgo=0
-	if [[ "$goos" == "ios" ]]; then cgo=1; fi # iOS builds require CGO, see https://github.com/golang/go/issues/43343
+  # iOS builds require Cgo, see https://github.com/golang/go/issues/43343
+  # Cgo would then need a C cross compilation setup. Not worth the hassle.
+	if [[ "$goos" == "ios" ]]; then continue; fi
+	# see https://github.com/golang/go/issues/43442
+	if [[ "$GOVERSION" == "1.16" && "$goos" == "openbsd" && "$goarch" == "mips64" ]]; then continue; fi
 
 	echo "$dist"
-	GOOS=$goos GOARCH=$goarch CGO_ENABLED=$cgo go build -o main example/main.go
+	GOOS=$goos GOARCH=$goarch go build -o main example/main.go
 	rm main
 done
