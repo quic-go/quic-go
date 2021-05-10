599d1813169fb2e12cf6788ee1e26295b03e8efe
Marten Seemann <martenseemann@gmail.com>
2021-05-10 16:48:56 -0700

    use tls.CipherSuiteName, instead of wrapping it in the qtls package

--- internal/qtls/go115.go
+++ internal/qtls/go115.go
@@ -10,7 +10,7 @@ import (
 	"net"
 	"unsafe"
 
-	qtls "github.com/marten-seemann/qtls-go1-15"
+	"github.com/marten-seemann/qtls-go1-15"
 )
 
 type (
@@ -52,11 +52,6 @@ const (
 	// EncryptionApplication is the application data encryption level
 	EncryptionApplication = qtls.EncryptionApplication
 )
-
-// CipherSuiteName gets the name of a cipher suite.
-func CipherSuiteName(id uint16) string {
-	return qtls.CipherSuiteName(id)
-}
 
 // HkdfExtract generates a pseudorandom key for use with Expand from an input secret and an optional independent salt.
 func HkdfExtract(hash crypto.Hash, newSecret, currentSecret []byte) []byte {
