22112b7b660e90b384eb206a4408eee6308b1ecc
Marten Seemann <martenseemann@gmail.com>
2021-05-10 16:44:08 -0700

    use HkdfExtract from x/crypto
    This means we don't have to expose it in qtls any more.

--- internal/handshake/initial_aead.go
+++ internal/handshake/initial_aead.go
@@ -4,6 +4,8 @@ import (
 	"crypto"
 	"crypto/tls"
 
+	"golang.org/x/crypto/hkdf"
+
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/qtls"
 )
@@ -49,7 +51,7 @@ func NewInitialAEAD(connID protocol.ConnectionID, pers protocol.Perspective, v p
 }
 
 func computeSecrets(connID protocol.ConnectionID, v protocol.VersionNumber) (clientSecret, serverSecret []byte) {
-	initialSecret := qtls.HkdfExtract(crypto.SHA256, connID, getSalt(v))
+	initialSecret := hkdf.Extract(crypto.SHA256.New, connID, getSalt(v))
 	clientSecret = hkdfExpandLabel(crypto.SHA256, initialSecret, []byte{}, "client in", crypto.SHA256.Size())
 	serverSecret = hkdfExpandLabel(crypto.SHA256, initialSecret, []byte{}, "server in", crypto.SHA256.Size())
 	return
