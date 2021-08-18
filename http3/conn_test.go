299bf195c8d11a9c9651dca48942f4cc781d5a8c
Randy Reddig <randy@alta.software>
2021-08-18 00:12:52 -0700

    http3: gofmt for golangci-lint

--- http3/conn_test.go
+++ http3/conn_test.go
@@ -8,27 +8,28 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("ServerConn", func() {
-	Context("Accept", func() {
-		It("fails when called on a client session", func() {
-			sess := mockquic.NewMockEarlySession(mockCtrl)
-			sess.EXPECT().Perspective().Return(quic.PerspectiveClient)
-			conn, err := Accept(sess, Settings{})
-			Expect(conn).To(BeNil())
-			Expect(err).To(HaveOccurred())
+var _ = Describe("Conn", func() {
+	Context("ServerConn", func() {
+		Context("Accept", func() {
+			It("fails when called on a client session", func() {
+				sess := mockquic.NewMockEarlySession(mockCtrl)
+				sess.EXPECT().Perspective().Return(quic.PerspectiveClient)
+				conn, err := Accept(sess, Settings{})
+				Expect(conn).To(BeNil())
+				Expect(err).To(HaveOccurred())
+			})
 		})
 	})
 
-})
-
-var _ = Describe("ClientConn", func() {
-	Context("Open", func() {
-		It("fails when called on a server session", func() {
-			sess := mockquic.NewMockEarlySession(mockCtrl)
-			sess.EXPECT().Perspective().Return(quic.PerspectiveServer)
-			conn, err := Open(sess, Settings{})
-			Expect(conn).To(BeNil())
-			Expect(err).To(HaveOccurred())
+	Context("ClientConn", func() {
+		Context("Open", func() {
+			It("fails when called on a server session", func() {
+				sess := mockquic.NewMockEarlySession(mockCtrl)
+				sess.EXPECT().Perspective().Return(quic.PerspectiveServer)
+				conn, err := Open(sess, Settings{})
+				Expect(conn).To(BeNil())
+				Expect(err).To(HaveOccurred())
+			})
 		})
 	})
 })
