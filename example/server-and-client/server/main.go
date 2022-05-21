9cbc2aa1b8b1fc5e994adf0334ce8f2760691a01
xieyuschen <xieyuschen@gmail.com>
2022-05-21 19:25:19 +0800

    add readme

--- example/server-and-client/server/main.go
+++ example/server-and-client/server/main.go
@@ -1,30 +1,91 @@
 package main
 
 import (
-	"bufio"
 	"crypto/md5"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"mime/multipart"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
+	"os/signal"
+	"path"
+	"runtime"
 	"strconv"
 	"strings"
-	"sync"
-
-	_ "net/http/pprof"
+	"syscall"
 
 	"github.com/lucas-clemente/quic-go"
 	"github.com/lucas-clemente/quic-go/http3"
-	"github.com/lucas-clemente/quic-go/internal/testdata"
-	"github.com/lucas-clemente/quic-go/internal/utils"
-	"github.com/lucas-clemente/quic-go/logging"
-	"github.com/lucas-clemente/quic-go/qlog"
 )
+
+var (
+	defaultServerDomain = "localhost:6121"
+	certFile, keyFile   string
+)
+
+func init() {
+	certFile, keyFile = func() (string, string) {
+		_, filename, _, ok := runtime.Caller(0)
+		if !ok {
+			panic("Failed to get current frame")
+		}
+
+		certPath := path.Dir(filename)
+		return path.Join(certPath, "/testdata/cert.pem"), path.Join(certPath, "/testdata/priv.key")
+
+	}()
+}
+func main() {
+	bs := binds{}
+	flag.Var(&bs, "bind", "bind to a certain domain")
+	www := flag.String("www", "", "www data")
+	tcp := flag.Bool("tcp", false, "also listen on TCP")
+	flag.Parse()
+
+	// set default serve port
+	if len(bs) == 0 {
+		bs = binds{defaultServerDomain}
+	}
+
+	handler := SetHandler(*www)
+	quicConf := &quic.Config{}
+
+	for _, b := range bs {
+		bCap := b
+		go func() {
+			var err error
+
+			if *tcp {
+				err = http3.ListenAndServe(bCap, certFile, keyFile, handler)
+			} else {
+				server := http3.Server{
+					Handler:    handler,
+					Addr:       bCap,
+					QuicConfig: quicConf,
+				}
+				err = server.ListenAndServeTLS(certFile, keyFile)
+			}
+			if err != nil {
+				fmt.Printf("failed to listen and serve at %s: %s\n", bCap, err)
+				os.Exit(-1)
+			}
+		}()
+	}
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT)
+	for {
+		s := <-ch
+		switch s {
+		case syscall.SIGINT:
+			fmt.Println("exit the ")
+			os.Exit(1)
+		}
+	}
+}
 
 type binds []string
 
@@ -42,6 +103,7 @@ type Size interface {
 	Size() int64
 }
 
+// generatePRData generates random by Lehmer algorithm,
 // See https://en.wikipedia.org/wiki/Lehmer_random_number_generator
 func generatePRData(l int) []byte {
 	res := make([]byte, l)
@@ -53,7 +115,7 @@ func generatePRData(l int) []byte {
 	return res
 }
 
-func setupHandler(www string) http.Handler {
+func SetHandler(www string) http.Handler {
 	mux := http.NewServeMux()
 
 	if len(www) > 0 {
@@ -121,7 +183,7 @@ func setupHandler(www string) http.Handler {
 					err = errors.New("couldn't get uploaded file size")
 				}
 			}
-			utils.DefaultLogger.Infof("Error receiving upload: %#v", err)
+			fmt.Printf("Error receiving upload: %#v", err)
 		}
 		io.WriteString(w, `<html><body><form action="/demo/upload" method="post" enctype="multipart/form-data">
 				<input type="file" name="uploadfile"><br>
@@ -130,72 +192,4 @@ func setupHandler(www string) http.Handler {
 	})
 
 	return mux
-}
-
-func main() {
-	// defer profile.Start().Stop()
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-	// runtime.SetBlockProfileRate(1)
-
-	verbose := flag.Bool("v", false, "verbose")
-	bs := binds{}
-	flag.Var(&bs, "bind", "bind to")
-	www := flag.String("www", "", "www data")
-	tcp := flag.Bool("tcp", false, "also listen on TCP")
-	enableQlog := flag.Bool("qlog", false, "output a qlog (in the same directory)")
-	flag.Parse()
-
-	logger := utils.DefaultLogger
-
-	if *verbose {
-		logger.SetLogLevel(utils.LogLevelDebug)
-	} else {
-		logger.SetLogLevel(utils.LogLevelInfo)
-	}
-	logger.SetLogTimeFormat("")
-
-	if len(bs) == 0 {
-		bs = binds{"localhost:6121"}
-	}
-
-	handler := setupHandler(*www)
-	quicConf := &quic.Config{}
-	if *enableQlog {
-		quicConf.Tracer = qlog.NewTracer(func(_ logging.Perspective, connID []byte) io.WriteCloser {
-			filename := fmt.Sprintf("server_%x.qlog", connID)
-			f, err := os.Create(filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("Creating qlog file %s.\n", filename)
-			return utils.NewBufferedWriteCloser(bufio.NewWriter(f), f)
-		})
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(bs))
-	for _, b := range bs {
-		bCap := b
-		go func() {
-			var err error
-			if *tcp {
-				certFile, keyFile := testdata.GetCertificatePaths()
-				err = http3.ListenAndServe(bCap, certFile, keyFile, handler)
-			} else {
-				server := http3.Server{
-					Handler:    handler,
-					Addr:       bCap,
-					QuicConfig: quicConf,
-				}
-				err = server.ListenAndServeTLS(testdata.GetCertificatePaths())
-			}
-			if err != nil {
-				fmt.Println(err)
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
 }
