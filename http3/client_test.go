6fbc6d951a4005d7d9d086118e1572b9e8ff9851
Marten Seemann <martenseemann@gmail.com>
2022-06-09 20:32:20 +0200

    fix flaky HTTP/3 request body test (#3447)

--- http3/client_test.go
+++ http3/client_test.go
@@ -797,9 +797,11 @@ var _ = Describe("Client", func() {
 					<-done
 					return 0, errors.New("test done")
 				})
-				str.EXPECT().Close()
+				closed := make(chan struct{})
+				str.EXPECT().Close().Do(func() { close(closed) })
 				_, err := client.RoundTripOpt(req, RoundTripOpt{})
 				Expect(err).To(MatchError("test done"))
+				Eventually(closed).Should(BeClosed())
 			})
 
 			It("sets the Content-Length", func() {
