cf1aff0492662aaa76d3e59cb15c8c16df2a9072
Avi Rosenberg <avrumi96@gmail.com>
2023-01-01 22:20:44 +0200

    http3: simplify if condition in roundtripper (#3658)
    Signed-off-by: Avi Rosenberg <avrumi96@gmail.com>
    
    Signed-off-by: Avi Rosenberg <avrumi96@gmail.com>

--- http3/roundtrip.go
+++ http3/roundtrip.go
@@ -110,21 +110,19 @@ func (r *RoundTripper) RoundTripOpt(req *http.Request, opt RoundTripOpt) (*http.
 		closeRequestBody(req)
 		return nil, errors.New("http3: nil Request.Header")
 	}
-
-	if req.URL.Scheme == "https" {
-		for k, vv := range req.Header {
-			if !httpguts.ValidHeaderFieldName(k) {
-				return nil, fmt.Errorf("http3: invalid http header field name %q", k)
-			}
-			for _, v := range vv {
-				if !httpguts.ValidHeaderFieldValue(v) {
-					return nil, fmt.Errorf("http3: invalid http header field value %q for key %v", v, k)
-				}
-			}
-		}
-	} else {
+	if req.URL.Scheme != "https" {
 		closeRequestBody(req)
 		return nil, fmt.Errorf("http3: unsupported protocol scheme: %s", req.URL.Scheme)
+	}
+	for k, vv := range req.Header {
+		if !httpguts.ValidHeaderFieldName(k) {
+			return nil, fmt.Errorf("http3: invalid http header field name %q", k)
+		}
+		for _, v := range vv {
+			if !httpguts.ValidHeaderFieldValue(v) {
+				return nil, fmt.Errorf("http3: invalid http header field value %q for key %v", v, k)
+			}
+		}
 	}
 
 	if req.Method != "" && !validMethod(req.Method) {
