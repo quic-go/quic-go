523036c4e63d1a3a14a691aefc0868d81a75c6e2
Marten Seemann <martenseemann@gmail.com>
2023-04-23 14:13:12 +0200

    ci: run go build jobs in parallel in cross compile job

--- .github/workflows/cross-compile.sh
+++ .github/workflows/cross-compile.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-for dist in $(go tool dist list); do
-	goos=$(echo $dist | cut -d "/" -f1)
-	goarch=$(echo $dist | cut -d "/" -f2)
-	# cross-compiling for android is a pain...
-	if [[ "$goos" == "android" ]]; then continue; fi
-  # iOS builds require Cgo, see https://github.com/golang/go/issues/43343
-  # Cgo would then need a C cross compilation setup. Not worth the hassle.
-	if [[ "$goos" == "ios" ]]; then continue; fi
+dist="$1"
+goos=$(echo "$dist" | cut -d "/" -f1)
+goarch=$(echo "$dist" | cut -d "/" -f2)
 
-	echo "$dist"
-	GOOS=$goos GOARCH=$goarch go build -o main example/main.go
-	rm main
-done
+# cross-compiling for android is a pain...
+if [[ "$goos" == "android" ]]; then exit; fi
+# iOS builds require Cgo, see https://github.com/golang/go/issues/43343
+# Cgo would then need a C cross compilation setup. Not worth the hassle.
+if [[ "$goos" == "ios" ]]; then exit; fi
+
+echo "$dist"
+out="main-$goos-$goarch"
+GOOS=$goos GOARCH=$goarch go build -o $out example/main.go
+rm $out
