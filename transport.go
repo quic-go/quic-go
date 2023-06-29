46025e5e301e878917bacabd54af81d4902a1bfc
kelmenhorst <k.elmenhorst@mailbox.org>
2023-06-29 18:56:33 +0200

    Transport: AddConn to multiplexer directly before start listening

--- transport.go
+++ transport.go
@@ -177,8 +177,6 @@ func (t *Transport) DialEarly(ctx context.Context, addr net.Addr, tlsConf *tls.C
 
 func (t *Transport) init(isServer bool) error {
 	t.initOnce.Do(func() {
-		getMultiplexer().AddConn(t.Conn)
-
 		var conn rawConn
 		if c, ok := t.Conn.(rawConn); ok {
 			conn = c
@@ -187,7 +185,6 @@ func (t *Transport) init(isServer bool) error {
 			conn, err = wrapConn(t.Conn)
 			if err != nil {
 				t.initErr = err
-				getMultiplexer().RemoveConn(t.Conn)
 				return
 			}
 		}
@@ -213,6 +210,7 @@ func (t *Transport) init(isServer bool) error {
 			t.connIDGenerator = &protocol.DefaultConnectionIDGenerator{ConnLen: t.connIDLen}
 		}
 
+		getMultiplexer().AddConn(t.Conn)
 		go t.listen(conn)
 		go t.runSendQueue()
 	})
