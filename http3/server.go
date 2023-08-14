70f3f44a0975ad84c96f6efa08f7ad47a39796eb
Marten Seemann <martenseemann@gmail.com>
2023-08-14 09:36:01 +0700

    http3: remove leftover ALPN constant for draft-29 (#4027)

--- http3/server.go
+++ http3/server.go
@@ -31,12 +31,8 @@ var (
 	}
 )
 
-const (
-	// NextProtoH3Draft29 is the ALPN protocol negotiated during the TLS handshake, for QUIC draft 29.
-	NextProtoH3Draft29 = "h3-29"
-	// NextProtoH3 is the ALPN protocol negotiated during the TLS handshake, for QUIC v1 and v2.
-	NextProtoH3 = "h3"
-)
+// NextProtoH3 is the ALPN protocol negotiated during the TLS handshake, for QUIC v1 and v2.
+const NextProtoH3 = "h3"
 
 // StreamType is the stream type of a unidirectional stream.
 type StreamType uint64
