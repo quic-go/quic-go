e3f533369b27074aa55ea0e9b8d9ac7ec5e342a3
Marten Seemann <martenseemann@gmail.com>
2023-09-04 11:23:58 +0700

    ackhandler: rename variables to follow RFC 9002 terminology

--- internal/ackhandler/interfaces.go
+++ internal/ackhandler/interfaces.go
@@ -44,7 +44,7 @@ type sentPacketTracker interface {
 // ReceivedPacketHandler handles ACKs needed to send for incoming packets
 type ReceivedPacketHandler interface {
 	IsPotentiallyDuplicate(protocol.PacketNumber, protocol.EncryptionLevel) bool
-	ReceivedPacket(pn protocol.PacketNumber, ecn protocol.ECN, encLevel protocol.EncryptionLevel, rcvTime time.Time, shouldInstigateAck bool) error
+	ReceivedPacket(pn protocol.PacketNumber, ecn protocol.ECN, encLevel protocol.EncryptionLevel, rcvTime time.Time, ackEliciting bool) error
 	DropPackets(protocol.EncryptionLevel)
 
 	GetAlarmTimeout() time.Time
