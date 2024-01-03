251be89a33399037dc2ff2d1a2349dbee9ab9cfb
Benedikt Spies <benedikt.spies@gmail.com>
2024-01-03 16:59:03 +0100

    add qlog default tracer which writes to QLOGDIR

---
+++ qlog/qlog_dir.go
@@ -0,0 +1,57 @@
+package qlog
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/quic-go/quic-go/internal/utils"
+	"github.com/quic-go/quic-go/logging"
+	"log"
+	"os"
+	"strings"
+)
+
+// QlogDir contains the value of the QLOGDIR environment variable.
+// If it is the empty string ("") no qlog output is written.
+var QlogDir string
+
+func init() {
+	QlogDir = os.Getenv("QLOGDIR")
+	if QlogDir != "" {
+		if _, err := os.Stat(QlogDir); os.IsNotExist(err) {
+			if err := os.MkdirAll(QlogDir, 0o755); err != nil {
+				log.Fatalf("failed to create qlog dir %s: %v", QlogDir, err)
+			}
+		}
+	}
+}
+
+// DefaultTracer creates a qlog file in the qlog directory specified by the QLOGDIR environment variable.
+// File names are <odcid>_<perspective>.qlog.
+// Returns nil if QLOGDIR is not set.
+func DefaultTracer(ctx context.Context, p logging.Perspective, connID logging.ConnectionID) *logging.ConnectionTracer {
+	var label string
+	switch p {
+	case logging.PerspectiveClient:
+		label = "client"
+	case logging.PerspectiveServer:
+		label = "server"
+	}
+	return qlogDirTracer(p, connID, label)
+}
+
+// qlogDirTracer creates a qlog file in the qlog directory specified by the QLOGDIR environment variable.
+// File names are <odcid>_<label>.qlog.
+// Returns nil if QLOGDIR is not set.
+func qlogDirTracer(p logging.Perspective, connID logging.ConnectionID, label string) *logging.ConnectionTracer {
+	if QlogDir == "" {
+		return nil
+	}
+	path := fmt.Sprintf("%s/%s_%s.qlog", strings.TrimRight(QlogDir, "/"), connID, label)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Printf("Failed to create qlog file %s: %s", path, err.Error())
+		return nil
+	}
+	return NewConnectionTracer(utils.NewBufferedWriteCloser(bufio.NewWriter(f), f), p, connID)
+}
---
+++ qlog/qlog_dir_test.go
@@ -0,0 +1,52 @@
+package qlog
+
+import (
+	"context"
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/quic-go/quic-go/internal/protocol"
+	"github.com/quic-go/quic-go/logging"
+	"os"
+)
+
+var _ = Describe("Tracing", func() {
+
+	var tempTestDirPath string
+	ctx := context.Background()
+	perspective := logging.PerspectiveClient
+	connID, _ := protocol.GenerateConnectionIDForInitial()
+
+	BeforeEach(func() {
+		var err error
+		tempTestDirPath, err = os.MkdirTemp("", "temp_test_dir")
+		fmt.Println(tempTestDirPath)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		QlogDir = ""
+		err := os.RemoveAll(tempTestDirPath)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("qlog dir ist set", func() {
+		QlogDir = tempTestDirPath
+		tracer := DefaultTracer(ctx, perspective, connID)
+		Expect(tracer).ToNot(BeNil())
+		tracer.Close()
+		_, err := os.Stat(tempTestDirPath)
+		qlogDirExist := !os.IsNotExist(err)
+		Expect(qlogDirExist).To(BeTrue())
+		childs, err := os.ReadDir(tempTestDirPath)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(len(childs)).To(Equal(1))
+	})
+
+	It("qlog dir is not set", func() {
+		QlogDir = ""
+		tracer := DefaultTracer(ctx, perspective, connID)
+		Expect(tracer).To(BeNil())
+	})
+
+})
