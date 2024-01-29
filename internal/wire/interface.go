be4838bd64e03c66a56755365409e68e8278f2cf
Marten Seemann <martenseemann@gmail.com>
2024-01-29 13:34:28 +0700

    wire: remove FrameParser interface, expose FrameParser struct (#4284)
    Instead, expose the FrameParser struct. This allows us to embed it
    directly into the connection struct, avoiding a pointer indirection.

--- internal/wire/interface.go
+++ internal/wire/interface.go
@@ -9,9 +9,3 @@ type Frame interface {
 	Append(b []byte, version protocol.VersionNumber) ([]byte, error)
 	Length(version protocol.VersionNumber) protocol.ByteCount
 }
-
-// A FrameParser parses QUIC frames, one by one.
-type FrameParser interface {
-	ParseNext([]byte, protocol.EncryptionLevel, protocol.VersionNumber) (int, Frame, error)
-	SetAckDelayExponent(uint8)
-}
