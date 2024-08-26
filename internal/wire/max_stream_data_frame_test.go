fab46397fcd04c8278250c0dcb9f90a15d057fab
Marten Seemann <martenseemann@gmail.com>
2024-08-26 11:39:45 +0800

    wire: translate MAX_STREAM_DATA frame tests

--- internal/wire/max_stream_data_frame_test.go
+++ internal/wire/max_stream_data_frame_test.go
@@ -2,59 +2,45 @@ package wire
 
 import (
 	"io"
+	"testing"
 
 	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/quicvarint"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("MAX_STREAM_DATA frame", func() {
-	Context("parsing", func() {
-		It("accepts sample frame", func() {
-			data := encodeVarInt(0xdeadbeef)                 // Stream ID
-			data = append(data, encodeVarInt(0x12345678)...) // Offset
-			frame, l, err := parseMaxStreamDataFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			Expect(frame.MaximumStreamData).To(Equal(protocol.ByteCount(0x12345678)))
-			Expect(l).To(Equal(len(data)))
-		})
+func TestParseMaxStreamFrame(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)                 // Stream ID
+	data = append(data, encodeVarInt(0x12345678)...) // Offset
+	frame, l, err := parseMaxStreamDataFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, protocol.StreamID(0xdeadbeef), frame.StreamID)
+	require.Equal(t, protocol.ByteCount(0x12345678), frame.MaximumStreamData)
+	require.Equal(t, len(data), l)
+}
 
-		It("errors on EOFs", func() {
-			data := encodeVarInt(0xdeadbeef)                 // Stream ID
-			data = append(data, encodeVarInt(0x12345678)...) // Offset
-			_, l, err := parseMaxStreamDataFrame(data, protocol.Version1)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(l).To(Equal(len(data)))
-			for i := range data {
-				_, _, err := parseMaxStreamDataFrame(data[:i], protocol.Version1)
-				Expect(err).To(MatchError(io.EOF))
-			}
-		})
-	})
+func TestParseMaxStreamDataErrorsOnEOFs(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)                 // Stream ID
+	data = append(data, encodeVarInt(0x12345678)...) // Offset
+	_, l, err := parseMaxStreamDataFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, len(data), l)
+	for i := range data {
+		_, _, err := parseMaxStreamDataFrame(data[:i], protocol.Version1)
+		require.Equal(t, io.EOF, err)
+	}
+}
 
-	Context("writing", func() {
-		It("has proper length", func() {
-			f := &MaxStreamDataFrame{
-				StreamID:          0x1337,
-				MaximumStreamData: 0xdeadbeef,
-			}
-			Expect(f.Length(protocol.Version1)).To(BeEquivalentTo(1 + quicvarint.Len(uint64(f.StreamID)) + quicvarint.Len(uint64(f.MaximumStreamData))))
-		})
-
-		It("writes a sample frame", func() {
-			f := &MaxStreamDataFrame{
-				StreamID:          0xdecafbad,
-				MaximumStreamData: 0xdeadbeefcafe42,
-			}
-			expected := []byte{maxStreamDataFrameType}
-			expected = append(expected, encodeVarInt(0xdecafbad)...)
-			expected = append(expected, encodeVarInt(0xdeadbeefcafe42)...)
-			b, err := f.Append(nil, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(b).To(Equal(expected))
-		})
-	})
-})
+func TestWriteMaxStreamDataFrame(t *testing.T) {
+	f := &MaxStreamDataFrame{
+		StreamID:          0xdecafbad,
+		MaximumStreamData: 0xdeadbeefcafe42,
+	}
+	expected := []byte{maxStreamDataFrameType}
+	expected = append(expected, encodeVarInt(0xdecafbad)...)
+	expected = append(expected, encodeVarInt(0xdeadbeefcafe42)...)
+	b, err := f.Append(nil, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, expected, b)
+	require.Equal(t, len(b), int(f.Length(protocol.Version1)))
+}
