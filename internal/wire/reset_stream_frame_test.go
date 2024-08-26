59fa3b9f81b07b5f42b42a86e623fb16b1c1f23d
Marten Seemann <martenseemann@gmail.com>
2024-08-26 11:55:22 +0800

    wire: translate RESET_STREAM frame tests

--- internal/wire/reset_stream_frame_test.go
+++ internal/wire/reset_stream_frame_test.go
@@ -1,66 +1,51 @@
 package wire
 
 import (
+	"testing"
+
 	"github.com/quic-go/quic-go/internal/protocol"
 	"github.com/quic-go/quic-go/internal/qerr"
-	"github.com/quic-go/quic-go/quicvarint"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("RESET_STREAM frame", func() {
-	Context("when parsing", func() {
-		It("accepts sample frame", func() {
-			data := encodeVarInt(0xdeadbeef)                  // stream ID
-			data = append(data, encodeVarInt(0x1337)...)      // error code
-			data = append(data, encodeVarInt(0x987654321)...) // byte offset
-			frame, l, err := parseResetStreamFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			Expect(frame.FinalSize).To(Equal(protocol.ByteCount(0x987654321)))
-			Expect(frame.ErrorCode).To(Equal(qerr.StreamErrorCode(0x1337)))
-			Expect(l).To(Equal(len(data)))
-		})
+func TestParseResetStream(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)                  // stream ID
+	data = append(data, encodeVarInt(0x1337)...)      // error code
+	data = append(data, encodeVarInt(0x987654321)...) // byte offset
+	frame, l, err := parseResetStreamFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, protocol.StreamID(0xdeadbeef), frame.StreamID)
+	require.Equal(t, protocol.ByteCount(0x987654321), frame.FinalSize)
+	require.Equal(t, qerr.StreamErrorCode(0x1337), frame.ErrorCode)
+	require.Equal(t, len(data), l)
+}
 
-		It("errors on EOFs", func() {
-			data := encodeVarInt(0xdeadbeef)                  // stream ID
-			data = append(data, encodeVarInt(0x1337)...)      // error code
-			data = append(data, encodeVarInt(0x987654321)...) // byte offset
-			_, l, err := parseResetStreamFrame(data, protocol.Version1)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(l).To(Equal(len(data)))
-			for i := range data {
-				_, _, err := parseResetStreamFrame(data[:i], protocol.Version1)
-				Expect(err).To(HaveOccurred())
-			}
-		})
-	})
+func TestParseResetStreamErrorsOnEOFs(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)                  // stream ID
+	data = append(data, encodeVarInt(0x1337)...)      // error code
+	data = append(data, encodeVarInt(0x987654321)...) // byte offset
+	_, l, err := parseResetStreamFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, len(data), l)
+	for i := range data {
+		_, _, err := parseResetStreamFrame(data[:i], protocol.Version1)
+		require.Error(t, err)
+	}
+}
 
-	Context("when writing", func() {
-		It("writes a sample frame", func() {
-			frame := ResetStreamFrame{
-				StreamID:  0x1337,
-				FinalSize: 0x11223344decafbad,
-				ErrorCode: 0xcafe,
-			}
-			b, err := frame.Append(nil, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			expected := []byte{resetStreamFrameType}
-			expected = append(expected, encodeVarInt(0x1337)...)
-			expected = append(expected, encodeVarInt(0xcafe)...)
-			expected = append(expected, encodeVarInt(0x11223344decafbad)...)
-			Expect(b).To(Equal(expected))
-		})
-
-		It("has the correct length", func() {
-			rst := ResetStreamFrame{
-				StreamID:  0x1337,
-				FinalSize: 0x1234567,
-				ErrorCode: 0xde,
-			}
-			expectedLen := 1 + quicvarint.Len(0x1337) + quicvarint.Len(0x1234567) + 2
-			Expect(rst.Length(protocol.Version1)).To(BeEquivalentTo(expectedLen))
-		})
-	})
-})
+func TestWriteResetStream(t *testing.T) {
+	frame := ResetStreamFrame{
+		StreamID:  0x1337,
+		FinalSize: 0x11223344decafbad,
+		ErrorCode: 0xcafe,
+	}
+	b, err := frame.Append(nil, protocol.Version1)
+	require.NoError(t, err)
+	expected := []byte{resetStreamFrameType}
+	expected = append(expected, encodeVarInt(0x1337)...)
+	expected = append(expected, encodeVarInt(0xcafe)...)
+	expected = append(expected, encodeVarInt(0x11223344decafbad)...)
+	require.Equal(t, expected, b)
+	require.Len(t, b, int(frame.Length(protocol.Version1)))
+}
