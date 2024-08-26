9eaadeda6c85ccc9842a5233271cf9663ac1e793
Marten Seemann <martenseemann@gmail.com>
2024-08-26 11:58:52 +0800

    wire: translate STREAM_DATA_BLOCKED frame tests

--- internal/wire/stream_data_blocked_frame_test.go
+++ internal/wire/stream_data_blocked_frame_test.go
@@ -2,59 +2,45 @@ package wire
 
 import (
 	"io"
+	"testing"
 
 	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/quicvarint"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("STREAM_DATA_BLOCKED frame", func() {
-	Context("parsing", func() {
-		It("accepts sample frame", func() {
-			data := encodeVarInt(0xdeadbeef)                 // stream ID
-			data = append(data, encodeVarInt(0xdecafbad)...) // offset
-			frame, l, err := parseStreamDataBlockedFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			Expect(frame.MaximumStreamData).To(Equal(protocol.ByteCount(0xdecafbad)))
-			Expect(l).To(Equal(len(data)))
-		})
+func TestParseStreamDataBlocked(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)                 // stream ID
+	data = append(data, encodeVarInt(0xdecafbad)...) // offset
+	frame, l, err := parseStreamDataBlockedFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, protocol.StreamID(0xdeadbeef), frame.StreamID)
+	require.Equal(t, protocol.ByteCount(0xdecafbad), frame.MaximumStreamData)
+	require.Equal(t, len(data), l)
+}
 
-		It("errors on EOFs", func() {
-			data := encodeVarInt(0xdeadbeef)
-			data = append(data, encodeVarInt(0xc0010ff)...)
-			_, l, err := parseStreamDataBlockedFrame(data, protocol.Version1)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(l).To(Equal(len(data)))
-			for i := range data {
-				_, _, err := parseStreamDataBlockedFrame(data[:i], protocol.Version1)
-				Expect(err).To(MatchError(io.EOF))
-			}
-		})
-	})
+func TestParseStreamDataBlockedErrorsOnEOFs(t *testing.T) {
+	data := encodeVarInt(0xdeadbeef)
+	data = append(data, encodeVarInt(0xc0010ff)...)
+	_, l, err := parseStreamDataBlockedFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, len(data), l)
+	for i := range data {
+		_, _, err := parseStreamDataBlockedFrame(data[:i], protocol.Version1)
+		require.Equal(t, io.EOF, err)
+	}
+}
 
-	Context("writing", func() {
-		It("has proper min length", func() {
-			f := &StreamDataBlockedFrame{
-				StreamID:          0x1337,
-				MaximumStreamData: 0xdeadbeef,
-			}
-			Expect(f.Length(0)).To(BeEquivalentTo(1 + quicvarint.Len(0x1337) + quicvarint.Len(0xdeadbeef)))
-		})
-
-		It("writes a sample frame", func() {
-			f := &StreamDataBlockedFrame{
-				StreamID:          0xdecafbad,
-				MaximumStreamData: 0x1337,
-			}
-			b, err := f.Append(nil, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			expected := []byte{streamDataBlockedFrameType}
-			expected = append(expected, encodeVarInt(uint64(f.StreamID))...)
-			expected = append(expected, encodeVarInt(uint64(f.MaximumStreamData))...)
-			Expect(b).To(Equal(expected))
-		})
-	})
-})
+func TestWriteStreamDataBlocked(t *testing.T) {
+	f := &StreamDataBlockedFrame{
+		StreamID:          0xdecafbad,
+		MaximumStreamData: 0x1337,
+	}
+	b, err := f.Append(nil, protocol.Version1)
+	require.NoError(t, err)
+	expected := []byte{streamDataBlockedFrameType}
+	expected = append(expected, encodeVarInt(uint64(f.StreamID))...)
+	expected = append(expected, encodeVarInt(uint64(f.MaximumStreamData))...)
+	require.Equal(t, expected, b)
+	require.Equal(t, int(f.Length(protocol.Version1)), len(b))
+}
