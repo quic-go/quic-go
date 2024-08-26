483aa0101bea6a7462132953d62f98d4f1513ae0
Marten Seemann <martenseemann@gmail.com>
2024-08-26 11:30:03 +0800

    wire: translate DATA_BLOCKED frame tests

--- internal/wire/data_blocked_frame_test.go
+++ internal/wire/data_blocked_frame_test.go
@@ -2,49 +2,39 @@ package wire
 
 import (
 	"io"
+	"testing"
 
 	"github.com/quic-go/quic-go/internal/protocol"
 	"github.com/quic-go/quic-go/quicvarint"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("DATA_BLOCKED frame", func() {
-	Context("when parsing", func() {
-		It("accepts sample frame", func() {
-			data := encodeVarInt(0x12345678)
-			frame, l, err := parseDataBlockedFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.MaximumData).To(Equal(protocol.ByteCount(0x12345678)))
-			Expect(l).To(Equal(len(data)))
-		})
+func TestParseDataBlocked(t *testing.T) {
+	data := encodeVarInt(0x12345678)
+	frame, l, err := parseDataBlockedFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, protocol.ByteCount(0x12345678), frame.MaximumData)
+	require.Equal(t, len(data), l)
+}
 
-		It("errors on EOFs", func() {
-			data := encodeVarInt(0x12345678)
-			_, l, err := parseDataBlockedFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(l).To(Equal(len(data)))
-			for i := range data {
-				_, _, err := parseDataBlockedFrame(data[:i], protocol.Version1)
-				Expect(err).To(MatchError(io.EOF))
-			}
-		})
-	})
+func TestParseDataBlockedErrorsOnEOFs(t *testing.T) {
+	data := encodeVarInt(0x12345678)
+	_, l, err := parseDataBlockedFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, len(data), l)
+	for i := range data {
+		_, _, err := parseDataBlockedFrame(data[:i], protocol.Version1)
+		require.Equal(t, io.EOF, err)
+	}
+}
 
-	Context("when writing", func() {
-		It("writes a sample frame", func() {
-			frame := DataBlockedFrame{MaximumData: 0xdeadbeef}
-			b, err := frame.Append(nil, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			expected := []byte{dataBlockedFrameType}
-			expected = append(expected, encodeVarInt(0xdeadbeef)...)
-			Expect(b).To(Equal(expected))
-		})
-
-		It("has the correct min length", func() {
-			frame := DataBlockedFrame{MaximumData: 0x12345}
-			Expect(frame.Length(protocol.Version1)).To(BeEquivalentTo(1 + quicvarint.Len(0x12345)))
-		})
-	})
-})
+func TestWriteDataBlocked(t *testing.T) {
+	frame := DataBlockedFrame{MaximumData: 0xdeadbeef}
+	b, err := frame.Append(nil, protocol.Version1)
+	require.NoError(t, err)
+	expected := []byte{dataBlockedFrameType}
+	expected = append(expected, encodeVarInt(0xdeadbeef)...)
+	require.Equal(t, expected, b)
+	require.Equal(t, protocol.ByteCount(1+quicvarint.Len(uint64(frame.MaximumData))), frame.Length(protocol.Version1))
+}
