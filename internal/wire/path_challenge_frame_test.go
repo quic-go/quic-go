5c2d39d01c32931d53f3954c7799d73153b7ee2e
Marten Seemann <martenseemann@gmail.com>
2024-08-26 11:52:19 +0800

    wire: translate PATH_CHALLENGE frame tests

--- internal/wire/path_challenge_frame_test.go
+++ internal/wire/path_challenge_frame_test.go
@@ -2,46 +2,36 @@ package wire
 
 import (
 	"io"
+	"testing"
 
 	"github.com/quic-go/quic-go/internal/protocol"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("PATH_CHALLENGE frame", func() {
-	Context("when parsing", func() {
-		It("accepts sample frame", func() {
-			b := []byte{1, 2, 3, 4, 5, 6, 7, 8}
-			f, l, err := parsePathChallengeFrame(b, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(f.Data).To(Equal([8]byte{1, 2, 3, 4, 5, 6, 7, 8}))
-			Expect(l).To(Equal(len(b)))
-		})
+func TestParsePathChallenge(t *testing.T) {
+	b := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+	f, l, err := parsePathChallengeFrame(b, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, [8]byte{1, 2, 3, 4, 5, 6, 7, 8}, f.Data)
+	require.Equal(t, len(b), l)
+}
 
-		It("errors on EOFs", func() {
-			data := []byte{1, 2, 3, 4, 5, 6, 7, 8}
-			_, l, err := parsePathChallengeFrame(data, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(l).To(Equal(len(data)))
-			for i := range data {
-				_, _, err := parsePathChallengeFrame(data[:i], protocol.Version1)
-				Expect(err).To(MatchError(io.EOF))
-			}
-		})
-	})
+func TestParsePathChallengeErrorsOnEOFs(t *testing.T) {
+	data := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+	_, l, err := parsePathChallengeFrame(data, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, len(data), l)
+	for i := range data {
+		_, _, err := parsePathChallengeFrame(data[:i], protocol.Version1)
+		require.Equal(t, io.EOF, err)
+	}
+}
 
-	Context("when writing", func() {
-		It("writes a sample frame", func() {
-			frame := PathChallengeFrame{Data: [8]byte{0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37}}
-			b, err := frame.Append(nil, protocol.Version1)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(b).To(Equal([]byte{pathChallengeFrameType, 0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37}))
-		})
-
-		It("has the correct length", func() {
-			frame := PathChallengeFrame{}
-			Expect(frame.Length(protocol.Version1)).To(Equal(protocol.ByteCount(9)))
-		})
-	})
-})
+func TestWritePathChallenge(t *testing.T) {
+	frame := PathChallengeFrame{Data: [8]byte{0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37}}
+	b, err := frame.Append(nil, protocol.Version1)
+	require.NoError(t, err)
+	require.Equal(t, []byte{pathChallengeFrameType, 0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37}, b)
+	require.Len(t, b, int(frame.Length(protocol.Version1)))
+}
