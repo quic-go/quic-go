4066efb7679e168534c528ecbb1ac608523c597f
Marten Seemann <martenseemann@gmail.com>
2024-09-14 14:34:20 +0800

    versionnegotiation: migrate tests away from Ginkgo

--- integrationtests/versionnegotiation/rtt_test.go
+++ integrationtests/versionnegotiation/rtt_test.go
@@ -2,54 +2,50 @@ package versionnegotiation
 
 import (
 	"context"
+	"testing"
 	"time"
 
 	"github.com/quic-go/quic-go"
 	quicproxy "github.com/quic-go/quic-go/integrationtests/tools/proxy"
 	"github.com/quic-go/quic-go/internal/protocol"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("Handshake RTT tests", func() {
-	const rtt = 400 * time.Millisecond
+const rtt = 400 * time.Millisecond
 
-	expectDurationInRTTs := func(startTime time.Time, num int) {
-		testDuration := time.Since(startTime)
-		rtts := float32(testDuration) / float32(rtt)
-		Expect(rtts).To(SatisfyAll(
-			BeNumerically(">=", num),
-			BeNumerically("<", num+1),
-		))
+func expectDurationInRTTs(t *testing.T, startTime time.Time, num int) {
+	t.Helper()
+	testDuration := time.Since(startTime)
+	rtts := float32(testDuration) / float32(rtt)
+	require.GreaterOrEqual(t, rtts, float32(num))
+	require.Less(t, rtts, float32(num+1))
+}
+
+func TestVersionNegotiationFailure(t *testing.T) {
+	if len(protocol.SupportedVersions) == 1 {
+		t.Fatal("Test requires at least 2 supported versions.")
 	}
 
-	It("fails when there's no matching version, after 1 RTT", func() {
-		if len(protocol.SupportedVersions) == 1 {
-			Skip("Test requires at least 2 supported versions.")
-		}
+	serverConfig := &quic.Config{}
+	serverConfig.Versions = protocol.SupportedVersions[:1]
+	ln, err := quic.ListenAddr("localhost:0", getTLSConfig(), serverConfig)
+	require.NoError(t, err)
+	defer ln.Close()
 
-		serverConfig := &quic.Config{}
-		serverConfig.Versions = protocol.SupportedVersions[:1]
-		ln, err := quic.ListenAddr("localhost:0", getTLSConfig(), serverConfig)
-		Expect(err).ToNot(HaveOccurred())
-		defer ln.Close()
-
-		// start the proxy
-		proxy, err := quicproxy.NewQuicProxy("localhost:0", &quicproxy.Opts{
-			RemoteAddr:  ln.Addr().String(),
-			DelayPacket: func(_ quicproxy.Direction, _ []byte) time.Duration { return rtt / 2 },
-		})
-		Expect(err).ToNot(HaveOccurred())
-
-		startTime := time.Now()
-		_, err = quic.DialAddr(
-			context.Background(),
-			proxy.LocalAddr().String(),
-			getTLSClientConfig(),
-			maybeAddQLOGTracer(&quic.Config{Versions: protocol.SupportedVersions[1:2]}),
-		)
-		Expect(err).To(HaveOccurred())
-		expectDurationInRTTs(startTime, 1)
+	// start the proxy
+	proxy, err := quicproxy.NewQuicProxy("localhost:0", &quicproxy.Opts{
+		RemoteAddr:  ln.Addr().String(),
+		DelayPacket: func(_ quicproxy.Direction, _ []byte) time.Duration { return rtt / 2 },
 	})
-})
+	require.NoError(t, err)
+
+	startTime := time.Now()
+	_, err = quic.DialAddr(
+		context.Background(),
+		proxy.LocalAddr().String(),
+		getTLSClientConfig(),
+		maybeAddQLOGTracer(&quic.Config{Versions: protocol.SupportedVersions[1:2]}),
+	)
+	require.Error(t, err)
+	expectDurationInRTTs(t, startTime, 1)
+}
