446bd86056fdbbb977b975f606f710a8dfb60205
Ainar Garipov <A.Garipov@AdGuard.COM>
2024-09-30 15:53:30 +0300

    utils: fix timer in Go 1.23
    See https://tip.golang.org/doc/go1.23#timer-changes.

--- internal/utils/timer.go
+++ internal/utils/timer.go
@@ -32,7 +32,12 @@ func (t *Timer) Reset(deadline time.Time) {
 	// We need to drain the timer if the value from its channel was not read yet.
 	// See https://groups.google.com/forum/#!topic/golang-dev/c9UUfASVPoU
 	if !t.t.Stop() && !t.read {
-		<-t.t.C
+		// Use a select to make sure that if there is no send on
+		// t.C (e.g. with Go 1.23), this doesn't block forever.
+		select {
+		case <-t.t.C:
+		default:
+		}
 	}
 	if !deadline.IsZero() {
 		t.t.Reset(time.Until(deadline))
