89752b01fff23fc601c8ee1161ed1bd785446ebe
WeidiDeng <weidi_deng@icloud.com>
2024-10-19 10:53:42 +0800

    close connHandlingDone once only when the server is shutting down

--- http3/server.go
+++ http3/server.go
@@ -222,6 +222,7 @@ type Server struct {
 	graceCtx         context.Context    // canceled when the server is closed or gracefully closed
 	graceCancel      context.CancelFunc // cancels the graceCtx
 	connCount        atomic.Int64
+	connDoneOnce     atomic.Bool
 	connHandlingDone chan struct{}
 
 	altSvcHeader string
@@ -285,7 +286,7 @@ func (s *Server) init() {
 }
 
 func (s *Server) decreaseConnCount() {
-	if s.connCount.Add(-1) == 0 {
+	if s.connCount.Add(-1) == 0 && s.graceCtx.Err() != nil && s.connDoneOnce.CompareAndSwap(false, true) {
 		close(s.connHandlingDone)
 	}
 }
