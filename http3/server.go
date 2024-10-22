98de6aebf7b139cd7ab3a93972ef9b9c1e378c1d
WeidiDeng <weidi_deng@icloud.com>
2024-10-22 13:35:35 +0800

    http3: fix graceful server shutdown (#4707)
    * close connHandlingDone once only when the server is shutting down
    
    * close underlying listeners during graceful  shutdown when there is no connection
    
    * remove connDoneOnce

--- http3/server.go
+++ http3/server.go
@@ -285,7 +285,7 @@ func (s *Server) init() {
 }
 
 func (s *Server) decreaseConnCount() {
-	if s.connCount.Add(-1) == 0 {
+	if s.connCount.Add(-1) == 0 && s.graceCtx.Err() != nil {
 		close(s.connHandlingDone)
 	}
 }
@@ -762,7 +762,7 @@ func (s *Server) Shutdown(ctx context.Context) error {
 	s.mutex.Unlock()
 
 	if s.connCount.Load() == 0 {
-		return nil
+		return s.Close()
 	}
 	select {
 	case <-s.connHandlingDone: // all connections were closed
