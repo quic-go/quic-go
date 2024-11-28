a50ccf75f6941157bbc036ad15a1c7e13cc797df
Marten Seemann <martenseemann@gmail.com>
2024-11-28 18:09:32 +0800

    only enable DF on macOS Sequoia (and newer) dual-stack sockets

--- sys_conn_df_darwin.go
+++ sys_conn_df_darwin.go
@@ -13,19 +13,22 @@ import (
 )
 
 func setDF(rawConn syscall.RawConn) (bool, error) {
-	// Setting DF bit is only supported from macOS11
+	// Setting DF bit is only supported from macOS 11.
 	// https://github.com/chromium/chromium/blob/117.0.5881.2/net/socket/udp_socket_posix.cc#L555
-	if supportsDF, err := isAtLeastMacOS11(); !supportsDF || err != nil {
+	version, err := getMacOSVersion()
+	if err != nil || version < 11 {
 		return false, err
 	}
 
 	var controlErr error
+	var disableDF bool
 	if err := rawConn.Control(func(fd uintptr) {
 		addr, err := unix.Getsockname(int(fd))
 		if err != nil {
 			controlErr = fmt.Errorf("getsockname: %w", err)
 			return
 		}
+
 		// Dual-stack sockets are effectively IPv6 sockets (with IPV6_ONLY set to 0).
 		// On macOS, the DF bit on dual-stack sockets is controlled by the IPV6_DONTFRAG option.
 		// See https://datatracker.ietf.org/doc/draft-seemann-tsvwg-udp-fragmentation/ for details.
@@ -34,13 +37,28 @@ func setDF(rawConn syscall.RawConn) (bool, error) {
 			controlErr = unix.SetsockoptInt(int(fd), unix.IPPROTO_IP, unix.IP_DONTFRAG, 1)
 		case *unix.SockaddrInet6:
 			controlErr = unix.SetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_DONTFRAG, 1)
+
+			// Setting the DF bit on dual-stack sockets works since macOS Sequoia.
+			// Disable DF on dual-stack sockets before Sequoia.
+			if version < 15 {
+				// check if this is a dual-stack socket by reading the IPV6_V6ONLY flag
+				v6only, err := unix.GetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_V6ONLY)
+				if err != nil {
+					controlErr = fmt.Errorf("getting IPV6_V6ONLY: %w", err)
+					return
+				}
+				disableDF = v6only == 0
+			}
 		default:
 			controlErr = fmt.Errorf("unknown address type: %T", addr)
 		}
 	}); err != nil {
 		return false, err
 	}
-	return controlErr == nil, controlErr
+	if controlErr != nil {
+		return false, controlErr
+	}
+	return !disableDF, nil
 }
 
 func isSendMsgSizeErr(err error) bool {
@@ -49,22 +67,22 @@ func isSendMsgSizeErr(err error) bool {
 
 func isRecvMsgSizeErr(error) bool { return false }
 
-func isAtLeastMacOS11() (bool, error) {
+// for macOS versions, see
+// https://en.wikipedia.org/wiki/Darwin_(operating_system)#Darwin_20_onwards
+func getMacOSVersion() (int, error) {
 	uname := &unix.Utsname{}
-	err := unix.Uname(uname)
-	if err != nil {
-		return false, err
+	if err := unix.Uname(uname); err != nil {
+		return 0, err
 	}
 
 	release := string(uname.Release[:])
-	if idx := strings.Index(release, "."); idx != -1 {
-		version, err := strconv.Atoi(release[:idx])
-		if err != nil {
-			return false, err
-		}
-		// Darwin version 20 is macOS version 11
-		// https://en.wikipedia.org/wiki/Darwin_(operating_system)#Darwin_20_onwards
-		return version >= 20, nil
+	idx := strings.Index(release, ".")
+	if idx == -1 {
+		return 0, nil
 	}
-	return false, nil
+	version, err := strconv.Atoi(release[:idx])
+	if err != nil {
+		return 0, err
+	}
+	return version, nil
 }
