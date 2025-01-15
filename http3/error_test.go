798895176fe859b7127d7e8db5c83c6aec2e7893
Marten Seemann <martenseemann@gmail.com>
2025-01-15 20:03:37 +0800

    http3: migrate the error tests away from Ginkgo

--- http3/error_test.go
+++ http3/error_test.go
@@ -2,40 +2,88 @@ package http3
 
 import (
 	"errors"
+	"testing"
 
 	"github.com/quic-go/quic-go"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/require"
 )
 
-var _ = Describe("HTTP/3 errors", func() {
-	It("converts", func() {
-		Expect(maybeReplaceError(nil)).To(BeNil())
-		Expect(maybeReplaceError(errors.New("foobar"))).To(MatchError("foobar"))
-		Expect(maybeReplaceError(&quic.StreamError{
-			ErrorCode: 1337,
-			Remote:    true,
-		})).To(Equal(&Error{
-			Remote:    true,
-			ErrorCode: 1337,
-		}))
-		Expect(maybeReplaceError(&quic.ApplicationError{
-			ErrorCode:    42,
-			Remote:       true,
-			ErrorMessage: "foobar",
-		})).To(Equal(&Error{
-			Remote:       true,
-			ErrorCode:    42,
-			ErrorMessage: "foobar",
-		}))
-	})
+func TestErrorConversion(t *testing.T) {
+	regularErr := errors.New("foobar")
 
-	It("has a string representation", func() {
-		Expect((&Error{ErrorCode: 0x10c, Remote: true}).Error()).To(Equal("H3_REQUEST_CANCELLED"))
-		Expect((&Error{ErrorCode: 0x10c, Remote: true, ErrorMessage: "foobar"}).Error()).To(Equal("H3_REQUEST_CANCELLED: foobar"))
-		Expect((&Error{ErrorCode: 0x10c, Remote: false}).Error()).To(Equal("H3_REQUEST_CANCELLED (local)"))
-		Expect((&Error{ErrorCode: 0x10c, Remote: false, ErrorMessage: "foobar"}).Error()).To(Equal("H3_REQUEST_CANCELLED (local): foobar"))
-		Expect((&Error{ErrorCode: 0x1337, Remote: true}).Error()).To(Equal("H3 error (0x1337)"))
-	})
-})
+	tests := []struct {
+		name     string
+		input    error
+		expected error
+	}{
+		{name: "nil error", input: nil, expected: nil},
+		{name: "regular error", input: regularErr, expected: regularErr},
+		{
+			name:     "stream error",
+			input:    &quic.StreamError{ErrorCode: 1337, Remote: true},
+			expected: &Error{Remote: true, ErrorCode: 1337},
+		},
+		{
+			name:     "application error",
+			input:    &quic.ApplicationError{ErrorCode: 42, Remote: true, ErrorMessage: "foobar"},
+			expected: &Error{Remote: true, ErrorCode: 42, ErrorMessage: "foobar"},
+		},
+		{
+			name:     "transport error",
+			input:    &quic.TransportError{ErrorCode: 42, Remote: true, ErrorMessage: "foobar"},
+			expected: &quic.TransportError{ErrorCode: 42, Remote: true, ErrorMessage: "foobar"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := maybeReplaceError(tt.input)
+			if tt.expected == nil {
+				require.Nil(t, result)
+			} else {
+				require.ErrorIs(t, tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestErrorString(t *testing.T) {
+	tests := []struct {
+		name     string
+		err      *Error
+		expected string
+	}{
+		{
+			name:     "remote error",
+			err:      &Error{ErrorCode: 0x10c, Remote: true},
+			expected: "H3_REQUEST_CANCELLED",
+		},
+		{
+			name:     "remote error with message",
+			err:      &Error{ErrorCode: 0x10c, Remote: true, ErrorMessage: "foobar"},
+			expected: "H3_REQUEST_CANCELLED: foobar",
+		},
+		{
+			name:     "local error",
+			err:      &Error{ErrorCode: 0x10c, Remote: false},
+			expected: "H3_REQUEST_CANCELLED (local)",
+		},
+		{
+			name:     "local error with message",
+			err:      &Error{ErrorCode: 0x10c, Remote: false, ErrorMessage: "foobar"},
+			expected: "H3_REQUEST_CANCELLED (local): foobar",
+		},
+		{
+			name:     "unknown error code",
+			err:      &Error{ErrorCode: 0x1337, Remote: true},
+			expected: "H3 error (0x1337)",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.expected, tt.err.Error())
+		})
+	}
+}
