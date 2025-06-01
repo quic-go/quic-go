f74f261317641648b9269e93b4c30c718b2cf1a2
Noste <83548733+Noooste@users.noreply.github.com>
2025-06-01 11:51:36 +0200

    refactor: restore clientHelloFn handling in crypto setup

--- internal/handshake/crypto_setup.go
+++ internal/handshake/crypto_setup.go
@@ -99,11 +99,11 @@ func NewCryptoSetupClient(
 
 	cs.conn.SetTransportParameters(cs.ourParams.Marshal(protocol.PerspectiveClient))
 
-	//if clientHelloFn == nil {
-	//	return cs
-	//}
-	//
-	//_ = cs.conn.ApplyPreset(clientHelloFn()) // uQuic-go TODO: better handle of error
+	if clientHelloFn == nil {
+		return cs
+	}
+
+	_ = cs.conn.ApplyPreset(clientHelloFn()) // uQuic-go TODO: better handle of error
 
 	return cs
 }
