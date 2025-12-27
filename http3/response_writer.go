a8027f5f70bab885a2275cddcefa45cb5c5d82eb
Marten Seemann <martenseemann@gmail.com>
2025-12-27 12:56:48 +0800

    http3: remove Hijacker

--- http3/response_writer.go
+++ http3/response_writer.go
@@ -52,7 +52,6 @@ type responseWriter struct {
 var (
 	_ http.ResponseWriter = &responseWriter{}
 	_ http.Flusher        = &responseWriter{}
-	_ Hijacker            = &responseWriter{}
 	_ Settingser          = &responseWriter{}
 	_ HTTPStreamer        = &responseWriter{}
 	// make sure that we implement (some of the) methods used by the http.ResponseController
@@ -336,10 +335,6 @@ func (w *responseWriter) HTTPStream() *Stream {
 }
 
 func (w *responseWriter) wasStreamHijacked() bool { return w.hijacked }
-
-func (w *responseWriter) Connection() *Conn {
-	return w.conn
-}
 
 func (w *responseWriter) ReceivedSettings() <-chan struct{} {
 	return w.conn.ReceivedSettings()
