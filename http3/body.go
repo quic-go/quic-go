9ea63f6e254c86f408e5e1bcfe72e6b70de103e1
ismailtrm <ismailtarim7@gmail.com>
2026-01-01 22:53:38 +0300

    http3: gofmt and minor formatting cleanup

--- http3/body.go
+++ http3/body.go
@@ -95,321 +95,97 @@ func newRequestBody(str *Stream, contentLength int64, connCtx context.Context, r
 }
 
 type hijackableBody struct {
-
 	body body
 
-
-
 	// only set for the http.Response
-
 	// The channel is closed when the user is done with this response:
-
 	// either when Read() errors, or when Close() is called.
-
 	reqDone     chan<- struct{}
-
 	reqDoneOnce sync.Once
 
-
-
 	mu                     sync.Mutex
-
 	ctx                    context.Context
-
 	dontCloseRequestStream bool
 
-
-
-		// closed when Close() is called
-
-
-
-		bodyClosed     chan struct{}
-
-
-
-		bodyClosedOnce sync.Once
-
-
-
-		monitorOnce    sync.Once
-
-
-
-	}
-
-
-
-	
-
-
-
-	var _ io.ReadCloser = &hijackableBody{}
-
-
-
-	
-
-
-
-	func newResponseBody(str *Stream, contentLength int64, done chan<- struct{}) *hijackableBody {
-
-
-
-		return &hijackableBody{
-
-
-
-			body:       *newBody(str, contentLength),
-
-
-
-			reqDone:    done,
-
-
-
-			bodyClosed: make(chan struct{}),
-
-
-
-		}
-
-
-
-	}
-
-
-
-	
-
-
-
-	func (r *hijackableBody) setContext(ctx context.Context, dontClose bool) {
-
-
-
-		r.mu.Lock()
-
-
-
-		r.ctx = ctx
-
-
-
-		r.dontCloseRequestStream = dontClose
-
-
-
-		r.mu.Unlock()
-
-
-
-	
-
-
-
-		if dontClose {
-
-
-
-			return
-
-
-
-		}
-
-
-
-	
-
-
-
-		r.monitorOnce.Do(func() {
-
-
-
-			go func() {
-
-
-
-				select {
-
-
-
-				case <-ctx.Done():
-
-
-
-					r.mu.Lock()
-
-
-
-					// Check again in case setContext was called again (unlikely) or dontClose changed
-
-
-
-					if !r.dontCloseRequestStream {
-
-
-
-						r.body.str.CancelRead(quic.StreamErrorCode(ErrCodeRequestCanceled))
-
-
-
-					}
-
-
-
-					r.mu.Unlock()
-
-
-
-				case <-r.bodyClosed:
-
-
-
-				}
-
-
-
-			}()
-
-
-
-		})
-
-
-
-	}
-
-
-
-	
-
-
-
-	func (r *hijackableBody) Read(b []byte) (int, error) {
-
-
-
-		r.mu.Lock()
-
-
-
-		ctx := r.ctx
-
-
-
-		dontClose := r.dontCloseRequestStream
-
-
-
-		r.mu.Unlock()
-
-
-
-	
-
-
-
-		if !dontClose && ctx != nil {
-
-
-
-			select {
-
-
-
-			case <-ctx.Done():
-
-
-
-				return 0, ctx.Err()
-
-
-
-			default:
-
-
-
-			}
-
-
-
-		}
-
-
-
-		n, err := r.body.Read(b)
-
-
-
-		if err != nil {
-
-
-
-			r.requestDone()
-
-
-
-		}
-
-
-
-		if n == 0 && err != nil && !dontClose && ctx != nil && ctx.Err() != nil {
-
-
-
-			return 0, ctx.Err()
-
-
-
-		}
-
-
-
-		return n, maybeReplaceError(err)
-
-
-
-	}
-
-
-
-	
-
-
-
-func (r *hijackableBody) requestDone() {
-
-	if r.reqDone != nil {
-
-		r.reqDoneOnce.Do(func() {
-
-			close(r.reqDone)
-
-		})
-
-	}
-
+	// closed when Close() is called
+	bodyClosed     chan struct{}
+	bodyClosedOnce sync.Once
+	monitorOnce    sync.Once
 }
 
+var _ io.ReadCloser = &hijackableBody{}
 
+func newResponseBody(str *Stream, contentLength int64, done chan<- struct{}) *hijackableBody {
+	return &hijackableBody{
+		body:       *newBody(str, contentLength),
+		reqDone:    done,
+		bodyClosed: make(chan struct{}),
+	}
+}
+
+func (r *hijackableBody) setContext(ctx context.Context, dontClose bool) {
+	r.mu.Lock()
+	r.ctx = ctx
+	r.dontCloseRequestStream = dontClose
+	r.mu.Unlock()
+
+	if dontClose {
+		return
+	}
+
+	r.monitorOnce.Do(func() {
+		go func() {
+			select {
+			case <-ctx.Done():
+				r.mu.Lock()
+				// Check again in case setContext was called again (unlikely) or dontClose changed
+				if !r.dontCloseRequestStream {
+					r.body.str.CancelRead(quic.StreamErrorCode(ErrCodeRequestCanceled))
+				}
+				r.mu.Unlock()
+			case <-r.bodyClosed:
+			}
+		}()
+	})
+}
+
+func (r *hijackableBody) Read(b []byte) (int, error) {
+	r.mu.Lock()
+	ctx := r.ctx
+	dontClose := r.dontCloseRequestStream
+	r.mu.Unlock()
+
+	if !dontClose && ctx != nil {
+		select {
+		case <-ctx.Done():
+			return 0, ctx.Err()
+		default:
+		}
+	}
+	n, err := r.body.Read(b)
+	if err != nil {
+		r.requestDone()
+	}
+	if n == 0 && err != nil && !dontClose && ctx != nil && ctx.Err() != nil {
+		return 0, ctx.Err()
+	}
+	return n, maybeReplaceError(err)
+}
+
+func (r *hijackableBody) requestDone() {
+	if r.reqDone != nil {
+		r.reqDoneOnce.Do(func() {
+			close(r.reqDone)
+		})
+	}
+}
 
 func (r *hijackableBody) Close() error {
-
 	r.bodyClosedOnce.Do(func() {
-
 		close(r.bodyClosed)
-
 	})
-
 	r.requestDone()
-
 	// If the EOF was read, CancelRead() is a no-op.
-
 	r.body.str.CancelRead(quic.StreamErrorCode(ErrCodeRequestCanceled))
-
 	return nil
-
 }
