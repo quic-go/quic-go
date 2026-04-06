51e0862811024771147c7ded0ff63140ae1222fd
Marten Seemann <martenseemann@gmail.com>
2026-04-06 22:42:58 +0800

    ci: remove obsolete -lresolv workaround for OSS-Fuzz

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,8 +3,6 @@
 go version
 go env
 
-export CXX="${CXX} -lresolv" # required by Go 1.20
-
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/frames Fuzz frame_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/header Fuzz header_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/transportparameters Fuzz transportparameter_fuzzer
