f0ad96b570793ef938d331b850e1437a98156d98
Marten Seemann <martenseemann@gmail.com>
2026-04-07 21:11:09 +0800

    wire: use Go native fuzzing for the frame parser (#5592)

--- oss-fuzz.sh
+++ oss-fuzz.sh
@@ -10,7 +10,7 @@ compile_go_fuzzer github.com/quic-go/qpack/fuzzing Fuzz qpack_fuzzer
 
 (
 # fuzz quic-go
-compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/frames Fuzz frame_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrameParser frame_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/header Fuzz header_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/transportparameters Fuzz transportparameter_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/tokens Fuzz token_fuzzer
@@ -26,7 +26,6 @@ cd $GOPATH/src/github.com/quic-go/quic-go/
 go generate -x ./fuzzing/...
 
 zip --quiet -r $OUT/header_fuzzer_seed_corpus.zip fuzzing/header/corpus
-zip --quiet -r $OUT/frame_fuzzer_seed_corpus.zip fuzzing/frames/corpus
 zip --quiet -r $OUT/transportparameter_fuzzer_seed_corpus.zip fuzzing/transportparameters/corpus
 zip --quiet -r $OUT/handshake_fuzzer_seed_corpus.zip fuzzing/handshake/corpus
 )
