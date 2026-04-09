4088d082729466969a19fa2f7f2c1d8e0a4ddbff
Marten Seemann <martenseemann@gmail.com>
2026-04-09 13:48:39 +0800

    wire: use Go native fuzzing for the transport parameter parser

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,7 +4,7 @@ go version
 go env
 
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrameParser frame_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzTransportParameters transportparameter_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/header Fuzz header_fuzzer
-compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/transportparameters Fuzz transportparameter_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/tokens Fuzz token_fuzzer
 compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/handshake Fuzz handshake_fuzzer
