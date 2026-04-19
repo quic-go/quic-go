616d3e065e29f9310e07897d5f41144e65522416
Marten Seemann <martenseemann@gmail.com>
2026-04-19 15:40:41 +0800

    handshake: use Go native fuzzing for the handshake fuzzer

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -7,5 +7,5 @@ compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFramePa
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzTransportParameters transportparameter_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzFrameParser http3_frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzHeaderParser header_fuzzer
-compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/handshake Fuzz handshake_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/handshake FuzzHandshake handshake_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzHeaderParsing http3_header_parsing_fuzzer
--- oss-fuzz.sh
+++ oss-fuzz.sh
@@ -3,34 +3,18 @@
 go version
 go env
 
-(
 # fuzz qpack
 cd $GOPATH/src/github.com/quic-go/qpack
-
 compile_native_go_fuzzer_v2 github.com/quic-go/qpack FuzzDecode qpack_decode_fuzzer
-)
 
-(
 # fuzz quic-go
 cd $GOPATH/src/github.com/quic-go/quic-go/
-
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrameParser frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzTransportParameters transportparameter_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzFrameParser http3_frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzHeaderParser header_fuzzer
-compile_go_fuzzer github.com/quic-go/quic-go/fuzzing/handshake Fuzz handshake_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/handshake FuzzHandshake handshake_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzHeaderParsing http3_header_parsing_fuzzer
-
-if [ $SANITIZER == "coverage" ]; then
-    # no need for corpora if coverage
-    exit 0
-fi
-
-# generate seed corpora
-go generate -x ./fuzzing/...
-
-zip --quiet -r $OUT/handshake_fuzzer_seed_corpus.zip fuzzing/handshake/corpus
-)
 
 # for debugging
 ls -al $OUT
