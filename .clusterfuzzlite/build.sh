d16adbe9da5335f888ae00c2fa6170dba8141bc2
Marten Seemann <martenseemann@gmail.com>
2026-04-22 08:28:05 -0700

    wire: fuzz splitting of CRYPTO and DATAGRAM, truncation of ACK frames

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,7 +3,7 @@
 go version
 go env
 
-compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrameParser frame_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrames frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzTransportParameters transportparameter_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzFrameParser http3_frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzHeaderParser header_fuzzer
--- oss-fuzz.sh
+++ oss-fuzz.sh
@@ -9,7 +9,7 @@ compile_native_go_fuzzer_v2 github.com/quic-go/qpack FuzzDecode qpack_decode_fuz
 
 # fuzz quic-go
 cd $GOPATH/src/github.com/quic-go/quic-go/
-compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrameParser frame_fuzzer
+compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzFrames frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzTransportParameters transportparameter_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/http3 FuzzFrameParser http3_frame_fuzzer
 compile_native_go_fuzzer_v2 github.com/quic-go/quic-go/internal/wire FuzzHeaderParser header_fuzzer
