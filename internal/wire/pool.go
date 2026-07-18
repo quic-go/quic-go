e161d20f100839347714df346141009b42584034
Marco Munizaga <git@marcopolo.io>
2026-07-17 18:27:35 -0700

    Support large STREAM frames over QMux

--- internal/wire/pool.go
+++ internal/wire/pool.go
@@ -6,7 +6,10 @@ import (
 	"github.com/quic-go/quic-go/internal/protocol"
 )
 
-var pool sync.Pool
+// STREAM frames are pooled in two buffer sizes:
+// protocol.MaxPacketBufferSize for frames that fit into a QUIC packet,
+// and protocol.MaxLargePacketBufferSize for the larger frames permitted in QMux records.
+var pool, largePool sync.Pool
 
 func init() {
 	pool.New = func() any {
@@ -15,19 +18,37 @@ func init() {
 			fromPool: true,
 		}
 	}
+	largePool.New = func() any {
+		return &StreamFrame{
+			Data:     make([]byte, 0, protocol.MaxLargePacketBufferSize),
+			fromPool: true,
+		}
+	}
 }
 
-func GetStreamFrame() *StreamFrame {
-	f := pool.Get().(*StreamFrame)
-	return f
+// GetStreamFrame gets a StreamFrame whose Data buffer has a capacity of at least size bytes.
+func GetStreamFrame(size protocol.ByteCount) *StreamFrame {
+	switch {
+	case size <= protocol.MaxPacketBufferSize:
+		return pool.Get().(*StreamFrame)
+	case size <= protocol.MaxLargePacketBufferSize:
+		return largePool.Get().(*StreamFrame)
+	default:
+		// This should never happen: frames are limited by the packet / record size.
+		return &StreamFrame{Data: make([]byte, 0, size)}
+	}
 }
 
 func putStreamFrame(f *StreamFrame) {
 	if !f.fromPool {
 		return
 	}
-	if protocol.ByteCount(cap(f.Data)) != protocol.MaxPacketBufferSize {
+	switch protocol.ByteCount(cap(f.Data)) {
+	case protocol.MaxPacketBufferSize:
+		pool.Put(f)
+	case protocol.MaxLargePacketBufferSize:
+		largePool.Put(f)
+	default:
 		panic("wire.PutStreamFrame called with packet of wrong size!")
 	}
-	pool.Put(f)
 }
